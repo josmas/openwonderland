d9effc979c7e77600badd32d563ae47240a4b2dd
paulby <paulby@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-03-31 19:24:20 +0000

    Comment out test case for png image that fails to load

--- core/src/classes/org/jdesktop/wonderland/client/jme/WonderlandJmeClient.java
+++ core/src/classes/org/jdesktop/wonderland/client/jme/WonderlandJmeClient.java
@@ -239,14 +239,15 @@ public class WonderlandJmeClient extends FixedFramerateGame implements PluginAcc
         renderInfo.getRoot().updateGeometricState(0.0f, true);
         renderInfo.getRoot().updateRenderState();
         
-        try {
-        InputStream in = new FileInputStream("/Users/paulby/src/java.net/jme/trunk/jme/src/jmetest/data/texture/Decal.PNG");
-        
-        TextureManager.loadImage("png", in, false);
-        } catch(Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
+        // Test case for problematic image format
+//        try {
+//        InputStream in = new FileInputStream("/Users/paulby/src/java.net/jme/trunk/jme/src/jmetest/data/texture/Decal.PNG");
+//        
+//        TextureManager.loadImage("png", in, false);
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//            System.exit(1);
+//        }
         
         isRunning = true;
     }
