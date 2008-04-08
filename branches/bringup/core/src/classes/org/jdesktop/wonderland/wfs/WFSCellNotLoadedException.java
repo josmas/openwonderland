7a83e2f0560ccb99632f4c706978c113479f6809
jslott <jslott@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-04-08 11:10:17 +0000

    Brought over WFS packages from v0.4 tree. Refactored into the org.jdesktop.wonderland.wfs package. The WFW v0.4 classes consist of a complete API for reading/writing a WFS to/from disk, jar files, and memory. Currently not used by any other code.

---
+++ branches/bringup/core/src/classes/org/jdesktop/wonderland/wfs/WFSCellNotLoadedException.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jdesktop.wonderland.wfs;
+
+/**
+ *
+ * @author jms54
+ */
+public class WFSCellNotLoadedException extends Exception {
+
+}
