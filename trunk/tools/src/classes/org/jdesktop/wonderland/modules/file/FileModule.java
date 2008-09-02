03612df8569e017d2ddae5c65b340d6a5002a654
jslott <jslott@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-09-02 21:25:20 +0000

    Added tools/ directory to trunk. This includes the WFS and Module API and Class libraries.

---
+++ trunk/tools/src/classes/org/jdesktop/wonderland/modules/file/FileModule.java
@@ -0,0 +1,117 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+
+package org.jdesktop.wonderland.modules.file;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import org.jdesktop.wonderland.checksum.RepositoryChecksums;
+import org.jdesktop.wonderland.modules.Module;
+import org.jdesktop.wonderland.modules.ModuleArtResource;
+import org.jdesktop.wonderland.modules.ModuleInfo;
+import org.jdesktop.wonderland.modules.ModulePlugin;
+import org.jdesktop.wonderland.modules.ModuleRepository;
+import org.jdesktop.wonderland.modules.ModuleRequires;
+import org.jdesktop.wonderland.modules.ModuleResource;
+import org.jdesktop.wonderland.wfs.WFS;
+
+/**
+ * The FileModule class extends the Module abstract base class and represents
+ * all modules that exist as a collection of directories and files on disk
+ * representing the structure of the module.
+ *
+ * @author Jordan Slott <jslott@dev.java.net>
+ */
+public class FileModule extends Module {
+    /* The root of the module directory structure on disk */
+    private File root = null;
+
+    
+    /** Default constructor, takes a reference to the module directory root */
+    public FileModule(File root) {
+        super();
+        this.root = root;
+    }
+    
+    /**
+     * Opens the module by reading its contents.
+     */
+    @Override
+    public void open() {
+        /* Fetch and parse the three XML files: info, requires, and repository */
+        ModuleInfo info = FileModuleUtil.parseModuleInfo(this.root);
+        if (info == null) {
+            // print error message XXX
+        }
+        
+        /* Read in the module */
+        ModuleRequires requires = FileModuleUtil.parseModuleRequires(this.root);
+        ModuleRepository repository = FileModuleUtil.parseModuleRepository(this.root);
+        HashMap<String, ModuleArtResource> artwork = FileModuleUtil.parseModuleArt(this.root);
+        HashMap<String, WFS> wfs = FileModuleUtil.parseModuleWFS(this.root);
+        HashMap<String, ModulePlugin> plugins = FileModuleUtil.parseModulePlugins(this.root);
+        RepositoryChecksums checksums = FileModuleUtil.parseModuleChecksums(this.root);
+        
+        /* Create a new module based upon what has been parsed */
+        this.setModuleInfo(info);
+        this.setModuleRequires(requires);
+        this.setModuleRepository(repository);
+        this.setModuleArtwork(artwork);
+        this.setModuleChecksums(checksums);
+        this.setModuleWFSs(wfs);
+        this.setModulePlugins(plugins);
+    }
+    
+    /**
+     * Returns an input stream for the given resource, null upon error.
+     *
+     * @param resource A resource contained within the archive
+     * @return An input stream to the resource
+     */
+    public InputStream getInputStreamForResource(ModuleResource resource) {
+        try {
+            String resourcePath = "art/" + resource.getPathName();
+            File entry = new File(this.root, resourcePath);
+            return new FileInputStream(entry);
+        } catch (java.io.IOException excp) {
+            // print stack trace
+            return null;
+        }
+    }
+    
+    /**
+     * Returns an input stream for the given JAR file from a plugin, null
+     * upon error
+     * 
+     * @param name The name of the plugin
+     * @param jar The name of the jar file
+     * @param type The type of the jar file (CLIENT, SERVER, COMMON)
+     */
+    public InputStream getInputStreamForPlugin(String name, String jar, String type) {
+        try {
+            String path = Module.MODULE_PLUGINS + "/" + name + "/" + type + jar;
+            File entry = new File(this.root, path);
+            return new FileInputStream(entry);
+        } catch (java.io.IOException excp) {
+            // print stack trace
+            return null;
+        }
+    }
+}
---
+++ trunk/tools/src/classes/org/jdesktop/wonderland/modules/jar/ModuleJarWriter.java
@@ -0,0 +1,211 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+
+package org.jdesktop.wonderland.modules.jar;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
+import javax.xml.bind.JAXBException;
+import org.jdesktop.wonderland.modules.Module;
+import org.jdesktop.wonderland.modules.ModuleArtResource;
+import org.jdesktop.wonderland.modules.ModulePlugin;
+import org.jdesktop.wonderland.wfs.WFS;
+
+/**
+ * The ModuleJarWriter class streams a Module to a JAR archive file. For
+ * certain module resources (artwork, plugin JARs), this class relies upon
+ * listeners to provide the input streams for each resources
+ * 
+ * @author Jordan Slott <jslott@dev.java.net>
+ */
+public class ModuleJarWriter {
+
+    /* Listener that returns an input stream for artwork resources */
+    private ModuleResourceListener resourceListener = null;
+    
+    /* Listener that returns an input stream for plugin JARs */
+    private ModulePluginListener pluginListener = null;
+    
+    /** Default constructor */
+    public ModuleJarWriter() {
+    }
+    
+    /**
+     * Sets the listener that returns an input stream for art resources.
+     * 
+     * @param resourceListener The listener for art resources
+     */
+    public void setResourceListener(ModuleResourceListener resourceListener) {
+        this.resourceListener = resourceListener;
+    }
+    
+    /**
+     * Sets the listener that returns an input stram for a plugin JAR.
+     * 
+     * @param pluginListener The listener for plugin JARs
+     */
+    public void setPluginListener(ModulePluginListener pluginListener) {
+        this.pluginListener = pluginListener;
+    }
+    
+        /**
+     * Stream this module out to an archive file given its output stream
+     * 
+     * @param os The output stream to write the module to
+     */
+    public void writeToJar(Module module, OutputStream os) throws IOException, JAXBException {
+        /* Make an output stream for the jar file */
+        JarOutputStream jos = new JarOutputStream(os);
+        
+        /* Write the basic module info, module.xml file */
+        if (module.getModuleInfo() != null) {
+            jos.putNextEntry(new ZipEntry(Module.MODULE_INFO));
+            module.getModuleInfo().encode(jos);
+        }
+        else {
+            throw new IOException("Module info may not be null");
+        }
+        
+        /* Write the module repository info, repository.xml file */
+        if (module.getModuleRepository() != null) {
+            jos.putNextEntry(new ZipEntry(Module.MODULE_REPOSITORY));
+            module.getModuleRepository().encode(jos);
+        }
+        
+        /* Write the module requirements, requires.xml file */
+        if (module.getModuleRequires() != null) {
+            jos.putNextEntry(new ZipEntry(Module.MODULE_REQUIRES));
+            module.getModuleRequires().encode(jos);
+        }
+ 
+        /* Write the module checksums, checksums.xml file */
+        if (module.getModuleChecksums() != null) {
+            jos.putNextEntry(new ZipEntry(Module.MODULE_CHECKSUMS));
+            module.getModuleChecksums().encode(jos);
+        }
+
+        /* Put an entry for the art/ directory */
+        jos.putNextEntry(new ZipEntry(Module.MODULE_ART + "/"));
+        
+        /*
+         * Write out the module artwork resources. Loop through each of the
+         * resources, ask the listener for the input stream for each resource
+         * and write out to the jar output stream.
+         */
+        Map<String, ModuleArtResource> resourceMap = module.getModuleArtwork();
+        Iterator<String> resourceIterator = resourceMap.keySet().iterator();
+        while (resourceIterator.hasNext() == true) {
+            /* Fetch the resource and an input stream to fetch the resource */
+            String resourceName = resourceIterator.next();
+            ModuleArtResource resource = resourceMap.get(resourceName);
+            InputStream is = this.resourceListener.getInputStreamForResource(resourceName);
+            
+            /* Does every directory need an entry? XXX */
+            
+            /* Write the entry to the JAR file */
+            jos.putNextEntry(new ZipEntry(Module.MODULE_ART + "/" + resourceName));
+            this.write(is, jos);
+        }
+        
+        /* Put an entry for the plugin/ directory */
+        jos.putNextEntry(new ZipEntry(Module.MODULE_PLUGINS + "/"));
+        
+        /*
+         * Write out each of the plugins. Loop through each of the plugins and
+         * ask the listener to write out each jar
+         */
+        Map<String, ModulePlugin> pluginMap = module.getModulePlugins();
+        Iterator<String> pluginIterator = pluginMap.keySet().iterator();
+        while (pluginIterator.hasNext() == true) {
+            /* Write an entry for the directory */
+            String pluginName = pluginIterator.next();
+            ModulePlugin plugin = pluginMap.get(pluginName);
+            jos.putNextEntry(new ZipEntry(Module.MODULE_PLUGINS + "/" + pluginName + "/"));
+            
+            /* Create entries for the client/, server/, and common/ directories */
+            jos.putNextEntry(new ZipEntry(Module.MODULE_PLUGINS + "/" + pluginName + "/" + ModulePlugin.CLIENT_JAR));
+            jos.putNextEntry(new ZipEntry(Module.MODULE_PLUGINS + "/" + pluginName + "/" + ModulePlugin.SERVER_JAR));
+            jos.putNextEntry(new ZipEntry(Module.MODULE_PLUGINS + "/" + pluginName + "/" + ModulePlugin.COMMON_JAR));
+
+            /* Fetch the list of client, server, and common jar file names */
+            Collection<String> clientJARs = plugin.getClientJars();
+            Collection<String> commonJARs = plugin.getCommonsJars();
+            Collection<String> serverJARs = plugin.getServerJars();
+            
+            /* Loop through all of the client JARs and write out */
+            Iterator<String> clientJARNames = clientJARs.iterator();
+            while (clientJARNames.hasNext() == true) {
+                /* Fetch the jar name and its input stream, write it out */
+                String clientName = clientJARNames.next();
+                InputStream is = this.pluginListener.getInputStreamForPluginJAR(clientName, ModulePlugin.CLIENT_JAR);
+                this.write(is, os);
+            }
+            
+            /* Loop through all of the server JARs and write out */
+            Iterator<String> serverJARNames = serverJARs.iterator();
+            while (serverJARNames.hasNext() == true) {
+                /* Fetch the jar name and its input stream, write it out */
+                String serverName = serverJARNames.next();
+                InputStream is = this.pluginListener.getInputStreamForPluginJAR(serverName, ModulePlugin.SERVER_JAR);
+                this.write(is, os);
+            }
+                        /* Loop through all of the client JARs and write out */
+            Iterator<String> commonJARNames = commonJARs.iterator();
+            while (commonJARNames.hasNext() == true) {
+                /* Fetch the jar name and its input stream, write it out */
+                String commonName = commonJARNames.next();
+                InputStream is = this.pluginListener.getInputStreamForPluginJAR(commonName, ModulePlugin.COMMON_JAR);
+                this.write(is, os);
+            }
+        }
+        
+        /*
+         * Write out each of the WFSs. Loop through the map, and write the
+         * directory structure to the JAR file
+         */
+        Map<String, WFS> wfsMap = module.getModuleWFSs();
+        Iterator<String> it = wfsMap.keySet().iterator();
+        while (it.hasNext() == true) {
+            String wfsName = it.next();
+            WFS wfs = wfsMap.get(wfsName);
+            wfs.writeTo(jos);
+        }
+    }
+    
+    /**
+     * Writes all of the data from the given input stream to the given output
+     * stream.
+     * 
+     * @param is The input stream to read data from
+     * @param os The output stream to write data to
+     * @throw IOException Upon general I/O error
+     */
+    private void write(InputStream is, OutputStream os) throws IOException {
+        int cc = -1;
+        byte buf[] = new byte[4 * 1028];
+        while ((cc = is.read(buf)) != -1) {
+            os.write(buf, 0, cc);
+        }
+    }
+}
