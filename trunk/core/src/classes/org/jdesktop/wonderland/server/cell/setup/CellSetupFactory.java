406fa8b0eb1dd4b00b6f19060f630a66656c44e2
jslott <jslott@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-09-24 02:27:16 +0000

    Initialize the JAXBContext class (for JAXB parsing and encoding) based upon information provided as services in JAR files. The services implement the CellSetupSPI interface and are listed in META-INF/ services. The list of fully-qualified class names that are annotated for JAXB are obtained from this list, plus a list of hard-coded core classes in the CellSetupFactory class. To include your setup class, have it implement CellSetupSPI (which has no methods) and put an entry in your JAR file as META-INF/services/org.jdesktop.wonderland.server.cell.setup.spi.CellSetupSPI that has the fully-qualified class names of your setup classes.

---
+++ trunk/core/src/classes/org/jdesktop/wonderland/server/cell/setup/CellSetupFactory.java
@@ -0,0 +1,111 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+
+package org.jdesktop.wonderland.server.cell.setup;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import org.jdesktop.wonderland.server.cell.setup.spi.CellSetupSPI;
+import sun.misc.Service;
+
+/**
+ * The CellSetupFactory returns marshallers and unmarshallers that can encode
+ * and decode XML that is bound to JAXB-annotated classes. This class uses
+ * Java's service provider mechanism to fetch the list of fully-qualified class
+ * names of Java objects that have JAXB annotations.
+ * <p>
+ * Classes that provide such a service must have an entry in the JAR file in
+ * which they are contained. In META-INF/services, a file named
+ * org.jdesktop.wonderland.server.cell.setup.CellSetupSPI should contain the
+ * fully-qualified class name(s) of all classes that implement the CellSetupSPI
+ * interface.
+ * 
+ * @author Jordan Slott <jslott@dev.java.net>
+ */
+public class CellSetupFactory {
+    /* A list of core cell setup class names */
+    private static String[] coreSetup = {
+        "org.jdesktop.wonderland.server.cell.setup.StaticModelCellSetup",
+        "org.jdesktop.wonderland.server.cell.setup.ColladaCellSetup"
+    };
+    
+    /* The XML marshaller and unmarshaller for later use */
+    private static Marshaller marshaller = null;
+    private static Unmarshaller unmarshaller = null;
+    
+    /* The Logger for this class */
+    private static Logger logger = Logger.getLogger(CellSetupFactory.class.getName());
+     
+    /* Create the XML marshaller and unmarshaller once for all setup classes */
+    static {
+        try {
+            /* Attempt to load the class names using the service providers */
+            Iterator<CellSetupSPI> it = Service.providers(CellSetupSPI.class);
+            Collection<Class> names = CellSetupFactory.getCoreCellSetup();
+            while (it.hasNext() == true) {
+                names.add(it.next().getClass());
+            }
+
+            JAXBContext jc = JAXBContext.newInstance(names.toArray(new Class[] {}));
+            CellSetupFactory.unmarshaller = jc.createUnmarshaller();
+            CellSetupFactory.marshaller = jc.createMarshaller();
+            CellSetupFactory.marshaller.setProperty("jaxb.formatted.output", true);
+        } catch (javax.xml.bind.JAXBException excp) {
+            CellSetupFactory.logger.log(Level.SEVERE, "[CELL] SETUP FACTORY Failed to create JAXBContext", excp);
+        }
+    }
+    
+    /**
+     * Returns the object that marshalls JAXB-annotated classes into XML
+     * 
+     * @return A marhsaller for JAXB-annotated classes
+     */
+    public static Marshaller getMarshaller() {
+        return CellSetupFactory.marshaller;
+    }
+    
+    /**
+     * Returns the object that unmarshalls XML into JAXB-annotated classes
+     * 
+     * @return An unmarshaller for XML
+     */
+    public static Unmarshaller getUnmarshaller() {
+        return CellSetupFactory.unmarshaller;
+    }
+    
+    /**
+     * Returns a collection of classes that represent the core cell setups.
+     */
+    private static Collection<Class> getCoreCellSetup() {
+        Collection<Class> list = new LinkedList<Class>();
+        for (String className : coreSetup) {
+            try {
+                list.add(Class.forName(className));
+            } catch (ClassNotFoundException excp) {
+                CellSetupFactory.logger.log(Level.WARNING, "[CELL] SETUP FACTORY Failed to find class", excp);
+            }
+        }
+        return list;
+    }
+}
