55ad14e292368f5bcf45b1db183073b74d57cbf7
paulby <paulby@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-09-29 21:49:51 +0000

    Refactor JME Collada loader from core to this module. Also refactor various temporary test cells out of core

---
+++ branches/paulby/dev/modules/world/jmecolladaloader/src/classes/org/jdesktop/wonderland/modules/jmecolladaloader/client/JmeColladaLoader.java
@@ -0,0 +1,269 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+package org.jdesktop.wonderland.modules.jmecolladaloader.client;
+
+import com.jme.scene.Node;
+import com.jme.util.resource.ResourceLocator;
+import com.jme.util.resource.ResourceLocatorTool;
+import com.jme.util.resource.SimpleResourceLocator;
+import com.jmex.model.collada.ColladaImporter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import org.jdesktop.wonderland.client.jme.artimport.ImportSessionFrame;
+import org.jdesktop.wonderland.client.jme.artimport.ModelLoader;
+import org.jdesktop.wonderland.client.protocols.wlzip.WlzipManager;
+import org.jdesktop.wonderland.modules.jmecolladaloader.common.cell.setup.JMEColladaCellSetup;
+
+/**
+ *
+ * Loader for SketchUp .kmz files
+ * 
+ * @author paulby
+ */
+class JmeColladaLoader implements ModelLoader {
+
+    private static final Logger logger = Logger.getLogger(JmeColladaLoader.class.getName());
+        
+    private HashMap<URL, ZipEntry> textureFiles = new HashMap();
+    
+    // The original file the user loaded
+    private File origFile;
+    
+    private ArrayList<String> modelFiles = new ArrayList();
+    
+    /**
+     * Load a SketchUP KMZ file and return the graph root
+     * @param file
+     * @return
+     */
+    public Node importModel(File file) throws IOException {
+        Node ret = null;
+        origFile = file;
+        
+//        ZipResourceLocator zipResource = new ZipResourceLocator(zipHost, zipFile);
+        SimpleResourceLocator resourceLocator = new SimpleResourceLocator(file.toURI());
+        ResourceLocatorTool.addResourceLocator(
+                ResourceLocatorTool.TYPE_TEXTURE,
+                resourceLocator);
+
+        
+        logger.info("Loading MODEL " + file.getName());
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
+        
+        ColladaImporter.load(in, file.getName());
+        ret = ColladaImporter.getModel();
+
+        ColladaImporter.cleanUp();
+        
+        ResourceLocatorTool.removeResourceLocator(ResourceLocatorTool.TYPE_TEXTURE, resourceLocator);
+
+        return ret;
+    }
+    
+
+    
+    public void deployToModule(File moduleRootDir) throws IOException {
+//        try {
+//            String modelName = origFile.getName();
+//            ZipFile zipFile = new ZipFile(origFile);
+//            
+//            // TODO replace getName with getModuleName(moduleRootDir)
+//            String moduleName = moduleRootDir.getName();
+//
+//            String targetDirName = moduleRootDir.getAbsolutePath()+File.separator+"art"+ File.separator + modelName;
+//            File targetDir = new File(targetDirName);
+//            targetDir.mkdir();
+//
+//            deployTextures(zipFile, targetDir);
+//            deployModels(zipFile, targetDir);
+//
+//            if (modelFiles.size() > 1) {
+//                logger.warning("Multiple models not supported during deploy");
+//            }
+//            ColladaCellSetup setup = new ColladaCellSetup();
+//            setup.setModel("wlm://"+moduleName+"/art/"+modelFiles.get(0));
+//            
+//            File wfsFile = new File(targetDirName+File.separator+"test.wfs");
+//            Writer w = new FileWriter(wfsFile);
+//            try {
+//                setup.encode(w);
+//            } catch (JAXBException ex) {
+//                logger.log(Level.SEVERE, null, ex);
+//            }
+//            w.close();
+//            
+//        } catch (ZipException ex) {
+//            logger.log(Level.SEVERE, null, ex);
+//            throw new IOException("Zip error");
+//        } catch (IOException ex) {
+//            logger.log(Level.SEVERE, null, ex);
+//            throw ex;
+//        }
+//        
+//        
+    }
+    
+    /**
+     * KMZ files keep all the models in the /models directory, copy all the
+     * models into the module
+     * @param moduleArtRootDir
+     */
+    private void deployModels(ZipFile zipFile, File targetDir) {
+        
+        // TODO update collada files with module relative texture paths
+        
+        try {
+            String targetDirName = targetDir.getAbsolutePath();
+            
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while(entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                if (entry.getName().endsWith(".dae")) {
+                    File target = new File(targetDirName+File.separator+entry.getName());
+                    target.getParentFile().mkdirs();
+                    target.createNewFile();
+                    
+                    copyAsset(zipFile, entry, target);
+                }
+            }
+            
+            
+        } catch (ZipException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    /**
+     * Deploys the textures into the art directory, placing them in a directory
+     * with the name of the original model.
+     * @param moduleArtRootDir
+     */
+    private void deployTextures(ZipFile zipFile, File targetDir) {
+        try {
+            // TODO generate checksums to check for image duplication
+            String targetDirName = targetDir.getAbsolutePath();
+
+            for (Map.Entry<URL, ZipEntry> t : textureFiles.entrySet()) {
+                File target = new File(targetDirName + File.separator + t.getKey().getPath());
+                target.getParentFile().mkdirs();
+                target.createNewFile();
+//                logger.fine("Texture file " + target.getAbsolutePath());
+                copyAsset(zipFile, t.getValue(), target);
+            }
+        } catch (ZipException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * Copy the asset from the zipEntry to the target file
+     * @param zipFile the zipFile that contains the zipEntry
+     * @param zipEntry entry to copy from
+     * @param target file to copy to
+     */
+    private void copyAsset(ZipFile zipFile, ZipEntry zipEntry, File target) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = new BufferedInputStream(zipFile.getInputStream(zipEntry));
+            out = new BufferedOutputStream(new FileOutputStream(target));
+            
+            org.jdesktop.wonderland.common.FileUtils.copyFile(in, out);
+            
+            
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (in!=null)
+                    in.close();
+                if (out!=null)
+                    out.close();
+            } catch (IOException ex) {
+                logger.log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    class ZipResourceLocator implements ResourceLocator {
+
+        private String zipHost;
+        private ZipFile zipFile;
+        
+        public ZipResourceLocator(String zipHost, ZipFile zipFile) {
+            this.zipHost = zipHost;
+            this.zipFile = zipFile;
+        }
+        
+        public URL locateResource(String filename) {
+            // Texture paths seem to be relative to the model directory....
+            if (filename.startsWith("../")) {
+                filename = filename.substring(3);
+            }
+            if (filename.startsWith("/")) {
+                filename = filename.substring(1);
+            }
+            
+            ZipEntry entry = zipFile.getEntry(filename);
+            if (entry==null) {
+                logger.severe("Unable to locate texture "+filename);
+                return null;
+            }
+            
+            try {
+                URL url = new URL("wlzip", zipHost, "/"+filename);
+                textureFiles.put(url, entry);
+                return url;
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(JmeColladaLoader.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return null;
+        }
+    }
+    
+
+}
---
+++ branches/paulby/dev/modules/world/jmecolladaloader/src/classes/org/jdesktop/wonderland/modules/jmecolladaloader/server/cell/JmeColladaCellMO.java
@@ -0,0 +1,105 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+package org.jdesktop.wonderland.modules.jmecolladaloader.server.cell;
+
+import org.jdesktop.wonderland.server.cell.*;
+import com.jme.bounding.BoundingBox;
+import com.jme.bounding.BoundingVolume;
+import com.jme.math.Vector3f;
+import com.sun.sgs.app.ClientSession;
+import org.jdesktop.wonderland.common.ExperimentalAPI;
+import org.jdesktop.wonderland.common.cell.CellTransform;
+import org.jdesktop.wonderland.common.cell.ClientCapabilities;
+import org.jdesktop.wonderland.common.cell.config.CellConfig;
+import org.jdesktop.wonderland.modules.jmecolladaloader.common.cell.config.ColladaCellConfig;
+import org.jdesktop.wonderland.common.cell.setup.BasicCellSetup;
+import org.jdesktop.wonderland.modules.jmecolladaloader.common.cell.setup.JMEColladaCellSetup;
+import org.jdesktop.wonderland.server.setup.BasicCellSetupHelper;
+import org.jdesktop.wonderland.server.setup.BeanSetupMO;
+
+
+/**
+ * A cell for collada models
+ * @author paulby
+ */
+@ExperimentalAPI
+public class JmeColladaCellMO extends CellMO implements BeanSetupMO { 
+    
+    /* The unique model URI */
+    private String modelURI = null;
+    	
+    /** Default constructor, used when cell is created via WFS */
+    public JmeColladaCellMO() {
+    }
+
+    public JmeColladaCellMO(Vector3f center, float size) {
+        super(new BoundingBox(new Vector3f(), size, size, size), new CellTransform(null, center));
+    }
+    
+    public JmeColladaCellMO(BoundingVolume bounds, CellTransform transform) {
+        super(bounds, transform);
+    }
+    
+    @Override protected String getClientCellClassName(ClientSession clientSession, ClientCapabilities capabilities) {
+        return "org.jdesktop.wonderland.modules.jmecolladaloader.client.cell.JmeColladaCell";
+    }
+
+    @Override
+    public CellConfig getCellConfig(ClientSession clientSession, ClientCapabilities capabilities) {
+        return new ColladaCellConfig(this.modelURI);
+    }
+
+    @Override
+    public void setupCell(BasicCellSetup setup) {
+        super.setupCell(setup);
+        this.modelURI = ((JMEColladaCellSetup)setup).getModel();
+    }
+
+    @Override
+    public void reconfigureCell(BasicCellSetup setup) {
+        super.reconfigureCell(setup);
+        setupCell(setup);
+    }
+
+     /**
+     * Return a new BasicCellSetup Java bean class that represents the current
+     * state of the cell.
+     * 
+     * @return a JavaBean representing the current state
+     */
+    public BasicCellSetup getCellMOSetup() {
+        /* Create a new BasicCellState and populate its members */
+        JMEColladaCellSetup setup = new JMEColladaCellSetup();
+        setup.setModel(this.modelURI);
+        
+        /* Set the bounds of the cell */
+        BoundingVolume bounds = this.getLocalBounds();
+        if (bounds != null) {
+            setup.setBounds(BasicCellSetupHelper.getSetupBounds(bounds));
+        }
+
+        /* Set the origin, scale, and rotation of the cell */
+        CellTransform transform = this.getLocalTransform(null);
+        if (transform != null) {
+            setup.setOrigin(BasicCellSetupHelper.getSetupOrigin(transform));
+            setup.setRotation(BasicCellSetupHelper.getSetupRotation(transform));
+            setup.setScaling(BasicCellSetupHelper.getSetupScaling(transform));
+        }
+        return setup;
+    }
+}
