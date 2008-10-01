d5454f6b3b13f84265deac63cf1553353aee3195
paulby <paulby@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-10-01 22:35:34 +0000

    A module for test cells.

---
+++ modules/world/testcells/src/classes/org/jdesktop/wonderland/modules/testcells/client/jme/cellrenderer/ShapeRenderer.java
@@ -0,0 +1,56 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+package org.jdesktop.wonderland.modules.testcells.client.jme.cellrenderer;
+
+import com.jme.math.Vector3f;
+import com.jme.scene.Node;
+import com.jme.scene.shape.Box;
+import com.jme.scene.shape.Cone;
+import com.jme.scene.shape.Sphere;
+import org.jdesktop.mtgame.Entity;
+import org.jdesktop.wonderland.client.cell.Cell;
+import org.jdesktop.wonderland.client.jme.cellrenderer.BasicRenderer;
+
+/**
+ * Render basic jme shapes
+ *
+ * @author paulby
+ */
+public class ShapeRenderer extends BasicRenderer {
+
+    public ShapeRenderer(Cell cell) {
+        super(cell);
+    }
+    
+    @Override
+    protected Node createSceneGraph(Entity entity) {
+        float xExtent = 1f;
+        float yExtent = 1f;
+        float zExtent = 1f;
+        
+        Node ret = new Node();
+        ret.setName(cell.getCellID().toString());
+        ret.attachChild(new Box("Box", new Vector3f(), xExtent, yExtent, zExtent));
+
+//        new Sphere("Sphere", 10, 10, xExtent);
+//        new Cone("Cone", 10, 10, xExtent, yExtent);s
+        
+        return ret;
+    }
+
+}
---
+++ modules/world/testcells/src/classes/org/jdesktop/wonderland/modules/testcells/server/cell/TestWorld.java
@@ -0,0 +1,52 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+package org.jdesktop.wonderland.modules.testcells.server.cell;
+
+import com.jme.bounding.BoundingBox;
+import com.jme.math.Vector3f;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jdesktop.wonderland.server.WonderlandContext;
+import org.jdesktop.wonderland.server.cell.CellManagerMO;
+import org.jdesktop.wonderland.server.ServerPlugin;
+
+/**
+ *
+ * @author paulby
+ */
+public class TestWorld implements ServerPlugin {
+
+    public TestWorld() {
+    }
+
+    public void initialize() {
+        try {
+
+            BoundingBox bounds = new BoundingBox(new Vector3f(), 1, 1, 1);
+
+            
+            //WonderlandContext.getCellManager().insertCellInWorld(new SimpleShapeCellMO(new Vector3f(7, 0, 5), 1));
+//            WonderlandContext.getCellManager().insertCellInWorld(new SkyboxHackMO(new Vector3f(45, 0, 5), 8));
+
+            WonderlandContext.getCellManager().insertCellInWorld(new MouseSpinCellMO(new Vector3f(5, 0, 5), 1));
+            
+        } catch (Exception ex) {
+            Logger.getLogger(CellManagerMO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
