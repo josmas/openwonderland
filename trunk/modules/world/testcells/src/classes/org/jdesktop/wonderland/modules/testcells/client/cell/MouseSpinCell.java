bb5999cecfa91f69630a7cb87080684a58ed7263
paulby <paulby@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-10-01 22:35:34 +0000

    A module for test cells.

---
+++ trunk/modules/world/testcells/src/classes/org/jdesktop/wonderland/modules/testcells/client/cell/MouseSpinCell.java
@@ -0,0 +1,57 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+package org.jdesktop.wonderland.modules.testcells.client.cell;
+
+import org.jdesktop.wonderland.client.cell.*;
+import org.jdesktop.wonderland.client.jme.cellrenderer.CellRendererJME;
+import org.jdesktop.wonderland.client.jme.input.test.SpinObjectEventListener;
+import org.jdesktop.wonderland.common.cell.CellID;
+import org.jdesktop.wonderland.modules.testcells.client.jme.cellrenderer.ShapeRenderer;
+
+/**
+ * Test for mouse over spin
+ * 
+ * @deprecated
+ * @author paulby
+ */
+public class MouseSpinCell extends Cell {
+    
+    public MouseSpinCell(CellID cellID, CellCache cellCache) {
+        super(cellID, cellCache);
+    }
+    
+    @Override
+    protected CellRenderer createCellRenderer(RendererType rendererType) {
+        CellRenderer ret = null;
+        switch(rendererType) {
+            case RENDERER_2D :
+                // No 2D Renderer yet
+                break;
+            case RENDERER_JME :
+                ret= new ShapeRenderer(this);
+                break;                
+        }
+        
+//        SpinObjectEventListener spinEventListener = new SpinObjectEventListener();
+//        spinEventListener.addToEntity(((CellRendererJME)ret).getEntity());
+                
+        return ret;
+    }
+    
+
+}
---
+++ trunk/modules/world/testcells/src/classes/org/jdesktop/wonderland/modules/testcells/client/jme/cellrenderer/SkyboxRenderer.java
@@ -0,0 +1,109 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+package org.jdesktop.wonderland.modules.testcells.client.jme.cellrenderer;
+
+import com.jme.image.Texture;
+import com.jme.scene.Skybox;
+import com.jme.scene.Spatial;
+import com.jme.scene.Spatial.TextureCombineMode;
+import com.jme.scene.state.CullState;
+import com.jme.scene.state.FogState;
+import com.jme.scene.state.RenderState;
+import com.jme.scene.state.ZBufferState;
+import com.jme.util.TextureManager;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jdesktop.mtgame.Entity;
+import org.jdesktop.mtgame.SkyboxComponent;
+import org.jdesktop.mtgame.WorldManager;
+import org.jdesktop.wonderland.client.cell.Cell;
+import org.jdesktop.wonderland.client.jme.ClientContextJME;
+import org.jdesktop.wonderland.client.jme.cellrenderer.CellRendererJME;
+import org.jdesktop.wonderland.common.cell.CellTransform;
+
+/**
+ *
+ * @author paulby
+ */
+public class SkyboxRenderer implements CellRendererJME {
+
+    private Entity entity = null;
+
+    public SkyboxRenderer(Cell cell) {
+
+    }
+
+    public Entity getEntity() {
+        if (entity==null)
+            entity = createEntity();
+        
+        return entity;
+    }
+    
+    private Entity createEntity() {
+        try {
+
+
+            WorldManager wm = ClientContextJME.getWorldManager();
+            Skybox skybox = new Skybox("skybox", 1000, 1000, 1000);
+            String dir = "jmetest/data/skybox1/";
+            Texture north = TextureManager.loadTexture(new URL("wlj://jmecolladaloader/skybox1/1.jpg"), Texture.MinificationFilter.BilinearNearestMipMap, Texture.MagnificationFilter.Bilinear);
+            Texture south = TextureManager.loadTexture(new URL("wlj://jmecolladaloader/skybox1/3.jpg"), Texture.MinificationFilter.BilinearNearestMipMap, Texture.MagnificationFilter.Bilinear);
+            Texture east = TextureManager.loadTexture(new URL("wlj://jmecolladaloader/skybox1/2.jpg"), Texture.MinificationFilter.BilinearNearestMipMap, Texture.MagnificationFilter.Bilinear);
+            Texture west = TextureManager.loadTexture(new URL("wlj://jmecolladaloader/skybox1/4.jpg"), Texture.MinificationFilter.BilinearNearestMipMap, Texture.MagnificationFilter.Bilinear);
+            Texture up = TextureManager.loadTexture(new URL("wlj://jmecolladaloader/skybox1/6.jpg"), Texture.MinificationFilter.BilinearNearestMipMap, Texture.MagnificationFilter.Bilinear);
+            Texture down = TextureManager.loadTexture(new URL("wlj://jmecolladaloader/skybox1/5.jpg"), Texture.MinificationFilter.BilinearNearestMipMap, Texture.MagnificationFilter.Bilinear);
+            skybox.setTexture(Skybox.Face.North, north);
+            skybox.setTexture(Skybox.Face.West, west);
+            skybox.setTexture(Skybox.Face.South, south);
+            skybox.setTexture(Skybox.Face.East, east);
+            skybox.setTexture(Skybox.Face.Up, up);
+            skybox.setTexture(Skybox.Face.Down, down);
+            //skybox.preloadTextures();
+            CullState cullState = (CullState) wm.getRenderManager().createRendererState(RenderState.RS_CULL);
+            cullState.setEnabled(true);
+            skybox.setRenderState(cullState);
+            ZBufferState zState = (ZBufferState) wm.getRenderManager().createRendererState(RenderState.RS_ZBUFFER);
+            //zState.setEnabled(false);
+            skybox.setRenderState(zState);
+            FogState fs = (FogState) wm.getRenderManager().createRendererState(RenderState.RS_FOG);
+            fs.setEnabled(false);
+            skybox.setRenderState(fs);
+            skybox.setLightCombineMode(Spatial.LightCombineMode.Off);
+            skybox.setCullHint(Spatial.CullHint.Never);
+            skybox.setTextureCombineMode(TextureCombineMode.Replace);
+            skybox.updateRenderState();
+            skybox.lockBounds();
+            //skybox.lockMeshes();
+            Entity e = new Entity("Skybox");
+            SkyboxComponent sbc = wm.getRenderManager().createSkyboxComponent(skybox, true);
+            e.addComponent(SkyboxComponent.class, sbc);
+            return e;
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(SkyboxRenderer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public void cellTransformUpdate(CellTransform cellLocal2World) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
