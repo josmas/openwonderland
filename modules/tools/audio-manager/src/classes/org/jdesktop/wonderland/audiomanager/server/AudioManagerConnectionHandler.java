326ad38415deff75a665ee68381c47b01463a478
jprovino <jprovino@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-10-07 19:47:40 +0000

    Fix PlaceCall message to have avatar position.

--- modules/tools/audio-manager/src/classes/org/jdesktop/wonderland/audiomanager/server/AudioManagerConnectionHandler.java
+++ modules/tools/audio-manager/src/classes/org/jdesktop/wonderland/audiomanager/server/AudioManagerConnectionHandler.java
@@ -32,9 +32,13 @@ import org.jdesktop.wonderland.audiomanager.common.AudioManagerConnectionType;
 
 import com.sun.sgs.app.AppContext;
 
-import com.sun.mpk20.voicelib.app.VoiceManager;
+import com.sun.mpk20.voicelib.app.AudioGroup;
+import com.sun.mpk20.voicelib.app.AudioGroupPlayerInfo;
 import com.sun.mpk20.voicelib.app.Call;
 import com.sun.mpk20.voicelib.app.CallSetup;
+import com.sun.mpk20.voicelib.app.Player;
+import com.sun.mpk20.voicelib.app.PlayerSetup;
+import com.sun.mpk20.voicelib.app.VoiceManager;
 
 import com.sun.voip.CallParticipant;
 
@@ -96,11 +100,37 @@ public class AudioManagerConnectionHandler
 
             vm.addCallStatusListener(this);
 
+	    Call call;
+
             try {
-                Call call = vm.createCall("jp", setup);
+                call = vm.createCall("jp", setup);
             } catch (IOException e) {
                 logger.warning("Unable to create call " + cp + ": " + e.getMessage());
+		return;
             }
+
+            PlayerSetup ps = new PlayerSetup();
+            ps.x = (double) msg.getX();
+            ps.y = (double) msg.getY();
+            ps.z = (double) msg.getZ();
+            ps.orientation =  getAngle(msg.getX(), msg.getY(), msg.getZ(),
+                msg.getDirection());
+            ps.isLivePlayer = true;
+
+            Player player = vm.createPlayer(call.getId(), ps);
+
+            call.setPlayer(player);
+            player.setCall(call);
+
+            vm.getDefaultLivePlayerAudioGroup().addPlayer(player,
+                new AudioGroupPlayerInfo(true, AudioGroupPlayerInfo.ChatType.PUBLIC));
+
+            AudioGroupPlayerInfo info = new AudioGroupPlayerInfo(false,
+                AudioGroupPlayerInfo.ChatType.PUBLIC);
+
+            info.defaultSpeakingAttenuation = 0;
+
+            vm.getDefaultStationaryPlayerAudioGroup().addPlayer(player, info);
 	} else if (message instanceof DisconnectCallMessage) {
 	    logger.warning("got DisconnectCallMessage");
 	} else {
@@ -111,4 +141,67 @@ public class AudioManagerConnectionHandler
     public void clientDisconnected(WonderlandClientSender sender, ClientSession session) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    /*
+     * XXX This probably doesn't belong here!
+     *
+     * Calculate the angle based on the direction vector
+     * We ignore the y value which is the up/down direction.
+     *
+     * The direction (-1, 0) is east or 0 degrees
+     *		     (0, 1) is north or 90 degrees
+     *	             (1, 0) is west or 180 degrees
+     *		     (0, -1 is south) or 270 degrees
+     */
+    public static double getAngle(double x, double y, double z, double direction) {
+	/*
+	 * x is given to us pointing in the wrong direction.
+	 */
+        x = -x;
+        
+        double angle;
+        
+        if (x == 0) {
+	    /*
+	     * x is 0, so we can't divide by x.
+	     * if z is non-negative, then the angle is 90.
+	     * Otherwise, it's 270.
+	     */
+            if (z < 0) {
+                angle = 90;
+            } else {
+                angle = 270;
+            }
+        } else {
+            angle = Math.toDegrees(Math.atan(z / x));
+            
+            if (x < 0) {
+          /*
+           * atan only produces a result between 0 and 180
+           * so we have to adjust it based on the z value
+           */
+                if (z > 0) {
+                    angle -= 180;
+                } else {
+                    angle += 180;
+                }
+            }
+        }
+        
+	/*
+	 * For wonderland, a clockwise rotation results in a bigger
+	 * angle, i. e., rotation is in the opposite direction of
+	 * what the voice manager expects.  Correct for that here.
+	 */
+        angle = 360 - angle;
+        
+        if (angle < 0) {
+            angle += 360;
+        }
+        
+        angle %= 360;
+        
+        return angle;
+    }
+
 }
