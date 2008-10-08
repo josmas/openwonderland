abbfea5fcbee8afce0fd49bd4cad6ad2589f67df
deronj <deronj@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-10-08 21:29:25 +0000

    Merge deronj/ab branch 613 into trunk. 1. Added new core modules: appbase, simplewhiteboard. 2. Got simplewhiteboard server and client cells to load and initialize.

---
+++ trunk/core/src/classes/org/jdesktop/wonderland/client/jme/utils/graphics/TexturedQuad.java
@@ -0,0 +1,210 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+
+package org.jdesktop.wonderland.client.jme.utils;
+
+import com.jme.bounding.BoundingBox;
+import com.jme.image.Texture;
+import com.jme.renderer.ColorRGBA;
+import com.jme.scene.TexCoords;
+import com.jme.scene.shape.Quad;
+import com.jme.scene.state.MaterialState;
+import com.jme.scene.state.RenderState;
+import com.jme.scene.state.TextureState;
+import com.jme.system.DisplaySystem;
+import com.jme.util.export.InputCapsule;
+import com.jme.util.export.JMEExporter;
+import com.jme.util.export.JMEImporter;
+import com.jme.util.export.OutputCapsule;
+import java.io.IOException;
+import java.nio.FloatBuffer;
+import com.jme.util.geom.BufferUtils;
+import org.jdesktop.wonderland.client.jme.utils.graphics.GraphicsUtils;
+import org.jdesktop.wonderland.common.ExperimentalAPI;
+
+/**
+ * A quad with a texture displayed on it.
+ * 
+ * @author deronj
+ */
+
+@ExperimentalAPI
+public class TexturedQuad extends Quad {
+    
+    /** Version number for serializing */
+    private static final long serialVersionUID = 1L;
+
+    /** The texture which is displayed on the quad. */
+    protected Texture texture;
+
+    /** The width in of the quad, in local coordinates. */
+    private float width;
+
+    /** The height in of the quad, in local coordinates. */
+    private float height;
+
+    /**
+     * Internal Only: Create a new instance of <code>TexturedQuad</code>. 
+     * The width and height attributes must be supplied later by calling <code>initialize</code>. 
+     *
+     * @param texture The texture to display on the quad.
+     */
+    public TexturedQuad (Texture texture) {
+	this(texture, "TempTexturedBox");
+    }
+
+    /**
+     * Create a new instance of <code>TexturedQuad</code> object. The texture is displayed on the front face only.
+     * The center and size vertice information must be supplied later. 
+     * 
+     * @param texture The texture to display on the quad.
+     * @param name The name of the scene element. 
+     */
+    public TexturedQuad (Texture texture, String name) {
+	super(name);
+	this.texture = texture;
+	initializeTexture();
+    }
+
+    /**
+     * Create a new instance of <code>TexturedQuad</code> object given a width and height. The quad is centered
+     * around its local origin.
+     * 
+     * @param texture The texture to display on the quad.
+     * @param name The name of the scene element. 
+     * @param width The width of the quad in local coordinates.
+     * @param height The height of the quad in local coordinates.
+     */
+    public TexturedQuad (Texture texture, String name, float width, float height) {
+	super(name, width, height);
+	this.texture = texture;
+	initializeTexture();
+    }
+
+    /**
+     * Specify a new texture.
+     */
+    public void setTexture (Texture texture) {
+	this.texture = texture;
+	TextureState ts = (TextureState) getRenderState(RenderState.RS_TEXTURE);
+	ts.setTexture(texture);
+    }
+
+    /**
+     * Returns this quad's texture.
+     */
+    public Texture getTexture () {
+	return texture;
+    }
+
+    /**
+     * Specify a new ambient-and-diffuse color of this quad.
+     */
+    public void setColor (ColorRGBA color) {
+	MaterialState ms = (MaterialState) getRenderState(RenderState.RS_MATERIAL);
+	if (ms == null) {
+	    ms = DisplaySystem.getDisplaySystem().getRenderer().createMaterialState();
+	    setRenderState(ms);
+	}
+	ms.setAmbient(new ColorRGBA(color));
+	ms.setDiffuse(new ColorRGBA(color));
+    }
+
+    /**
+     * Returns the ambient-and-diffuse color of this quad. 
+     */
+    public ColorRGBA getColor () {
+	MaterialState ms = (MaterialState) getRenderState(RenderState.RS_MATERIAL);
+	if (ms == null) {
+	    return new ColorRGBA(1f, 1f, 1f, 1f);
+	} else {
+	    return ms.getDiffuse();
+	}
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initialize (float width, float height) {
+	super.initialize(width, height);
+	initializeTexture();
+	this.width = width;
+	this.height = height;
+    }
+
+    /**
+     * Initialize texture attributes.
+     */
+    private void initializeTexture () {
+
+	if (getRenderState(RenderState.RS_TEXTURE) == null) {
+	    TextureState ts = DisplaySystem.getDisplaySystem().getRenderer().createTextureState();
+	    ts.setTexture(texture);
+	    setRenderState(ts);
+	    setModelBound(new BoundingBox());
+	    updateModelBound();
+	}
+
+        FloatBuffer tbuf = BufferUtils.createVector2Buffer(4);
+        setTextureCoords(new TexCoords(tbuf));
+	tbuf.put(0).put(0);
+	tbuf.put(0).put(1);
+	tbuf.put(1).put(1);
+	tbuf.put(1).put(0);
+    }
+
+    /**
+     * Creates a new TexturedQuad object containing the same data as this one.
+     * 
+     * @return The new TexturedQuad.
+     */
+    public Object clone() {
+	TexturedQuad rVal = new TexturedQuad(texture, getName() + "_clone", width, height);
+	return rVal;
+    }
+
+    public void write(JMEExporter e) throws IOException {
+        super.write(e);
+        OutputCapsule capsule = e.getCapsule(this);
+	// TODO: texture attrs
+    }
+
+    public void read(JMEImporter e) throws IOException {
+        super.read(e);
+        InputCapsule capsule = e.getCapsule(this);
+	// TODO: texture attrs
+    }
+
+    /**
+     * For debug: Print the contents of this object's render state.
+     */
+    public void printRenderState () {
+	MaterialState ms = (MaterialState) getRenderState(RenderState.RS_MATERIAL);
+	GraphicsUtils.printRenderState(ms);
+	TextureState ts = (TextureState) getRenderState(RenderState.RS_TEXTURE);
+	GraphicsUtils.printRenderState(ts);
+    }    
+
+    /**
+     * For debug: Print the contents of this object's geometry
+     */
+    public void printGeometry () {
+	GraphicsUtils.printGeometry(this);
+    }    
+}
---
+++ trunk/modules/foundation/appbase/src/classes/org/jdesktop/wonderland/modules/appbase/common/AppConventionalMessage.java
@@ -0,0 +1,101 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+package org.jdesktop.wonderland.modules.appbase.common;
+
+import org.jdesktop.wonderland.common.messages.Message;
+import com.jme.bounding.BoundingVolume;
+import com.jme.math.Vector2f;
+import org.jdesktop.wonderland.common.cell.CellTransform;
+import java.io.Serializable;
+import java.util.UUID;
+import org.jdesktop.wonderland.common.InternalAPI;
+
+/**
+ * Superclass for messages that the App Base uses to communicate between the client and server
+ * about conventional cells.
+ * 
+ * @author deronj
+ */
+
+@InternalAPI
+public abstract class AppConventionalMessage extends Message {
+
+    /** The action type of this message */
+    protected ActionType actionType;
+
+    /**
+     * The types of app conventional messages.
+     *
+     * CELL_CREATE: Client tells the server to create a new app cell.
+     */
+    public enum ActionType { CELL_CREATE };
+
+    /** The default constructor */
+    public AppConventionalMessage () {}
+
+    /**
+     * Creates a new instance of AppConventionalMessage.
+     * 
+     * @param actionType The type of the message.
+     */
+    public AppConventionalMessage (ActionType actionType) {
+        this.actionType = actionType;
+    }
+
+    /**
+     * Specify the action type of this message.
+     * @param actionType The action type of this message.
+     */
+    public void setActionType (ActionType actionType) {
+        this.actionType = actionType;
+    }
+
+    /**
+     * Return the action type of this message.
+     * @return The action type of this message.
+     */
+    public ActionType getActionType() {
+        return actionType;
+    }
+
+    /**
+     * Create a new CELL_CREATE message.
+     *
+     * @param appTypeName The name of the app type.
+     * @param masterHost The name of the master host.
+     * @param appName The name of the app instance.
+     * @param appId The app ID.
+     * @param bestView True if the window should be should be moved into best view on startup.
+     * @param bounds The bounds of the app cell.
+     * @param transform The app cell transform.
+     * @param pixelScale The size of the pixels in world coordinates.
+     * @param connectionInfo Subclass-specific data for making a peer-to-peer connection between master and slave.
+     * @return Returns a new CELL_CREATE message.
+     */
+    public static AppConventionalCellCreateMessage newCellCreateMessage (String appTypeName, String masterHost, 
+								 String appName, UUID appId, boolean bestView, 
+								 BoundingVolume bounds, CellTransform transform, 
+								 Vector2f pixelScale, Serializable connectionInfo) {
+        AppConventionalCellCreateMessage ret = new AppConventionalCellCreateMessage(ActionType.CELL_CREATE, appTypeName, 
+									  masterHost, appName, appId, bestView, 
+									  bounds, transform, pixelScale, 
+									  connectionInfo);
+        return ret;
+    }
+}
+
