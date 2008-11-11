a0e6e6cbc86c030ddb065593514fb9f7ac9bcdc4
paulby <paulby@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-11-11 22:59:33 +0000

    A default Environment, sets the global lights and skybox

---
+++ trunk/modules/world/defaultenvironment/src/classes/org/jdesktop/wonderland/modules/defaultenvironment/client/jme/ClientInit.java
@@ -0,0 +1,36 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+package org.jdesktop.wonderland.modules.defaultenvironment.client.jme;
+
+import org.jdesktop.wonderland.client.ClientPlugin;
+import org.jdesktop.wonderland.client.comms.WonderlandSession;
+import org.jdesktop.wonderland.client.jme.ClientContextJME;
+import org.jdesktop.wonderland.client.login.LoginManager;
+
+/**
+ *
+ * @author paulby
+ */
+public class ClientInit implements ClientPlugin {
+
+    public void initialize(LoginManager loginManager) {
+        ClientContextJME.getEnvironmentManager().addEnvironment(loginManager, "Default", new DefaultEnvironment(loginManager));
+        ClientContextJME.getEnvironmentManager().setCurrentEnvironment(loginManager, "Default");
+    }
+
+}
