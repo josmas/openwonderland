f9c02bc580650b0a2babdf220cbf8b353395ca5d
paulby <paulby@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-11-18 22:35:02 +0000

    Add avatar animations to menu for testing

--- trunk/modules/world/avatarbase/src/classes/org/jdesktop/wonderland/modules/avatarbase/client/jme/cellrenderer/AvatarPlugin.java
+++ trunk/modules/world/avatarbase/src/classes/org/jdesktop/wonderland/modules/avatarbase/client/jme/cellrenderer/AvatarPlugin.java
@@ -17,8 +17,20 @@
  */
 package org.jdesktop.wonderland.modules.avatarbase.client.jme.cellrenderer;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Logger;
+import javax.swing.ButtonGroup;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
 import org.jdesktop.wonderland.client.ClientPlugin;
+import org.jdesktop.wonderland.client.cell.Cell;
+import org.jdesktop.wonderland.client.cell.CellRenderer;
+import org.jdesktop.wonderland.client.cell.view.ViewCell;
 import org.jdesktop.wonderland.client.jme.ClientContextJME;
+import org.jdesktop.wonderland.client.jme.JmeClientMain;
+import org.jdesktop.wonderland.client.jme.ViewManager.ViewManagerListener;
 import org.jdesktop.wonderland.client.login.ServerSessionManager;
 
 /**
@@ -29,6 +41,59 @@ public class AvatarPlugin implements ClientPlugin {
 
     public void initialize(ServerSessionManager loginManager) {
         ClientContextJME.getAvatarRenderManager().registerRenderer(AvatarImiJME.class);
+        
+        ClientContextJME.getViewManager().addViewManagerListener(new ViewManagerListener() {
+            public void primaryViewCellChanged(ViewCell oldViewCell, ViewCell newViewCell) {
+                if (oldViewCell!=null) {
+                    Logger.getAnonymousLogger().severe("TODO Handle primary View Change");
+                    return;
+                }
+
+                CellRenderer rend = newViewCell.getCellRenderer(Cell.RendererType.RENDERER_JME);
+                if (!(rend instanceof AvatarImiJME))
+                    return;
+
+                final AvatarImiJME avatar = (AvatarImiJME)rend;
+                String[] animations = avatar.getAnimations();
+
+                JMenu animationMenu = new JMenu("Avatar Actions");
+
+                ButtonGroup buttonGroup = new ButtonGroup();
+
+                for(int i=0; i<animations.length; i++) {
+                    JRadioButtonMenuItem m = new JRadioButtonMenuItem(animations[i]);
+                    buttonGroup.add(m);
+                    animationMenu.add(m);
+                    m.addActionListener(new ActionListener() {
+
+                        public void actionPerformed(ActionEvent e) {
+                            avatar.setAnimation(((JMenuItem)e.getSource()).getText());
+                        }
+                    });
+                }
+
+                JMenuItem startMI = new JMenuItem("Start Animation");
+                startMI.addActionListener(new ActionListener() {
+
+                    public void actionPerformed(ActionEvent e) {
+                        avatar.triggerActionStart();
+                    }
+                });
+
+                JMenuItem stopMI = new JMenuItem("Stop Animation");
+                stopMI.addActionListener(new ActionListener() {
+
+                    public void actionPerformed(ActionEvent e) {
+                        avatar.triggerActionStop();
+                    }
+                });
+
+                animationMenu.add(startMI);
+                animationMenu.add(stopMI);
+
+                JmeClientMain.getFrame().addToEditMenu(animationMenu);
+            }
+        });
     }
 
 }
