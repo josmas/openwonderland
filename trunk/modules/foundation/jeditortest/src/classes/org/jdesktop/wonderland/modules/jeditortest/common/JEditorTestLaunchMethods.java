e8eaf32eb69d7a460f34be23db081d0a5200eb81
deronj <deronj@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-12-16 00:03:55 +0000

    Add new test for JEditorPanel

---
+++ trunk/modules/foundation/jeditortest/src/classes/org/jdesktop/wonderland/modules/jeditortest/common/JEditorTestLaunchMethods.java
@@ -0,0 +1,37 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+package org.jdesktop.wonderland.modules.jeditortest.common;
+
+import org.jdesktop.wonderland.common.ExperimentalAPI;
+import org.jdesktop.wonderland.modules.appbase.common.AppLaunchMethods;
+import org.jdesktop.wonderland.modules.appbase.common.AppLaunchMethodsWonderland;
+
+/**
+ * The JEditor test is a Wonderland app. It currently is only launched from the world. 
+ *
+ * @author deronj
+ */
+
+@ExperimentalAPI
+public class JEditorTestLaunchMethods extends AppLaunchMethodsWonderland {
+
+    /** Create an instance of JEditorTestAppLaunchMethods */
+    public JEditorTestLaunchMethods () {
+	addLauncher(AppLaunchMethods.Launcher.WORLD);
+    }
+}
