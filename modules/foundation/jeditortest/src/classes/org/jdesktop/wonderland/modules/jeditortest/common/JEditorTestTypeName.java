acb63c90ca663c33c1a617b820a5f192c04eb0ae
deronj <deronj@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-12-16 00:03:55 +0000

    Add new test for JEditorPanel

---
+++ modules/foundation/jeditortest/src/classes/org/jdesktop/wonderland/modules/jeditortest/common/JEditorTestTypeName.java
@@ -0,0 +1,35 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+package org.jdesktop.wonderland.modules.jeditortest.common;
+
+import org.jdesktop.wonderland.common.ExperimentalAPI;
+
+/**
+ * The external app type name for the JEditor test app. 
+ *
+ * @author deronj
+ */
+
+@ExperimentalAPI
+public class JEditorTestTypeName {
+
+    /**
+     * The external app type name for the JEditor test app. 
+     */
+    public static final String JEDITOR_TEST_APP_TYPE_NAME = "JEditorTest";
+}
