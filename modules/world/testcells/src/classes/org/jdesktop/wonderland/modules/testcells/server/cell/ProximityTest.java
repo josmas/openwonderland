d698c770b013c0318dcc0f1e9ed5f576f56b3010
paulby <paulby@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-12-22 20:01:07 +0000

    Proximity Example

---
+++ modules/world/testcells/src/classes/org/jdesktop/wonderland/modules/testcells/server/cell/ProximityTest.java
@@ -0,0 +1,33 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
+ */
+package org.jdesktop.wonderland.modules.testcells.server.cell;
+
+import com.jme.bounding.BoundingVolume;
+import org.jdesktop.wonderland.common.cell.CellID;
+import org.jdesktop.wonderland.server.cell.ProximityListenerSrv;
+
+/**
+ *
+ * @author paulby
+ */
+public class ProximityTest implements ProximityListenerSrv {
+    public void viewEnterExit(boolean entered, CellID cell, BoundingVolume proximityVolume, int proximityIndex) {
+        System.err.println("ENter/Exit "+entered);
+    }
+
+}
