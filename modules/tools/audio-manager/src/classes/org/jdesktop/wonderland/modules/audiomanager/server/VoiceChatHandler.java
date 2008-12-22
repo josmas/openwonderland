0982850faeff120e03f920b7cc34db0f1dc8a62a
jprovino <jprovino@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2008-12-22 19:13:14 +0000

    Fix voicemanger problems with the x-coodinate increasing to the left instead of the right.

--- modules/tools/audio-manager/src/classes/org/jdesktop/wonderland/modules/audiomanager/server/VoiceChatHandler.java
+++ modules/tools/audio-manager/src/classes/org/jdesktop/wonderland/modules/audiomanager/server/VoiceChatHandler.java
@@ -637,7 +637,7 @@ public class VoiceChatHandler implements TransformChangeListenerSrv,
 
 	    Vector3f location = localToWorldTransform.getTranslation(null);
 	
-	    logger.fine(player + " x " + -location.getX()
+	    logger.fine(player + " x " + location.getX()
 		+ " y " + location.getY() + " z " + location.getZ()
 		+ " angle " + angle);
 
@@ -645,7 +645,7 @@ public class VoiceChatHandler implements TransformChangeListenerSrv,
 	     * The x coordinate increases to the left so we correct
 	     * for that here.
 	     */
-	    player.moved(-location.getX(), location.getY(), location.getZ(), angle);
+	    player.moved(location.getX(), location.getY(), location.getZ(), angle);
 	}
     }
 
