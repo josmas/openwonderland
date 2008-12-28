8880c6725bf8af091b2b2a69395027d401a87c05
jonathankap <jonathankap@users.noreply.github.com>
2008-12-28 22:51:39 +0000

    Initial creation of scripting branch

---
+++ scripting/core/src/classes/org/jdesktop/wonderland/client/jme/ThirdPersonCameraProcessor.java
@@ -0,0 +1,131 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath" 
+ * exception as provided by Sun in the License file that accompanied 
+ * this code.
+ */
+package org.jdesktop.wonderland.client.jme;
+
+import com.jme.math.Quaternion;
+import com.jme.math.Vector3f;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import org.jdesktop.mtgame.ProcessorArmingCollection;
+import org.jdesktop.mtgame.WorldManager;
+import org.jdesktop.wonderland.client.input.Event;
+import org.jdesktop.wonderland.client.input.EventClassFocusListener;
+import org.jdesktop.wonderland.client.jme.input.KeyEvent3D;
+import org.jdesktop.wonderland.client.jme.input.MouseEvent3D;
+import org.jdesktop.wonderland.common.cell.CellTransform;
+
+/**
+ * A very simplistic third person camera model
+ * 
+ * @author paulby
+ */
+public class ThirdPersonCameraProcessor extends CameraProcessor {
+
+    private Quaternion rotation = new Quaternion();
+    private Vector3f translation = new Vector3f();
+    protected float cameraZoom = 0.2f;
+    protected Vector3f offset = new Vector3f(0,2.2f,-6);
+    private boolean commitRequired = false;
+    private Quaternion viewRot = new Quaternion();
+    private Vector3f viewTranslation = new Vector3f();
+
+    protected Vector3f cameraLook = new Vector3f(0,0,1);
+    private Vector3f yUp = new Vector3f(0,1,0);
+
+    private Vector3f tmp=new Vector3f();
+
+    private WorldManager wm;
+    
+    public ThirdPersonCameraProcessor() {
+        wm = ClientContextJME.getWorldManager();
+
+        // TODO this should be done in setEnabled
+        ClientContextJME.getInputManager().addGlobalEventListener(new EventClassFocusListener() {
+            @Override
+            public Class[] eventClassesToConsume () {
+                return new Class[] { KeyEvent3D.class, MouseEvent3D.class };
+            }
+
+            @Override
+            public void commitEvent (Event event) {
+                if (event instanceof KeyEvent3D) {
+                    KeyEvent key = (KeyEvent) ((KeyEvent3D)event).getAwtEvent();
+                    if (key.getKeyCode()==KeyEvent.VK_EQUALS) {
+                        offset.z += cameraZoom;
+                        viewMoved(new CellTransform(viewRot, viewTranslation));
+                    } else if (key.getKeyCode()==KeyEvent.VK_MINUS) {
+                        offset.z -= cameraZoom;
+                        viewMoved(new CellTransform(viewRot, viewTranslation));
+                    }
+                } else if (event instanceof MouseEvent3D) {
+                    MouseEvent mouse = (MouseEvent)((MouseEvent3D)event).getAwtEvent();
+                    if (mouse instanceof MouseWheelEvent) {
+                        int clicks = ((MouseWheelEvent)mouse).getWheelRotation();
+                        offset.z -= cameraZoom*clicks;
+                        viewMoved(new CellTransform(viewRot, viewTranslation));
+                    }
+                }
+            }
+        });
+    }
+    
+    @Override
+    public void compute(ProcessorArmingCollection arg0) {
+    }
+
+    @Override
+    public void commit(ProcessorArmingCollection arg0) {
+        synchronized(this) {
+            if (commitRequired) {
+                cameraNode.setLocalRotation(rotation);
+                cameraNode.setLocalTranslation(translation);
+                wm.addToUpdateList(cameraNode);
+                commitRequired = false;
+            }
+
+        }
+    }
+
+    @Override
+    public void initialize() {
+        // Camera is chained so does not need it's own ArmingCondition
+    }
+
+    @Override
+    public void viewMoved(CellTransform worldTransform) {
+        synchronized(this) {
+            translation = worldTransform.getTranslation(translation);
+            tmp = translation.clone();
+            rotation = worldTransform.getRotation(rotation);
+            viewRot.set(rotation);
+            viewTranslation.set(translation);
+
+
+            Vector3f cameraTrans = rotation.mult(offset);
+//            System.out.println("Camera trans "+cameraTrans );
+            translation.addLocal(cameraTrans);
+            commitRequired=true;
+
+            rotation.lookAt(rotation.mult(cameraLook), yUp);
+        }
+    }
+
+
+
+}
---
+++ scripting/modules/tools/audio-manager/src/classes/org/jdesktop/wonderland/modules/audiomanager/server/AudioTreatmentComponentMO.java
@@ -0,0 +1,181 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2008, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath" 
+ * exception as provided by Sun in the License file that accompanied 
+ * this code.
+ */
+package org.jdesktop.wonderland.modules.audiomanager.server;
+
+import com.jme.math.Quaternion;
+import com.jme.math.Vector3f;
+import com.jme.scene.shape.Quad;
+import com.sun.sgs.app.AppContext;
+import com.sun.sgs.app.ClientSession;
+import com.sun.sgs.app.DataManager;
+import com.sun.sgs.app.ManagedObject;
+import com.sun.sgs.app.ManagedReference;
+import com.sun.sgs.app.Task;
+import com.sun.sgs.app.TaskManager;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.logging.Logger;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.jdesktop.wonderland.common.cell.CellTransform;
+import org.jdesktop.wonderland.common.cell.messages.CellMessage;
+
+import org.jdesktop.wonderland.common.cell.setup.BasicCellSetup;
+import org.jdesktop.wonderland.common.cell.setup.CellComponentSetup;
+
+import org.jdesktop.wonderland.server.TimeManager;
+import org.jdesktop.wonderland.server.WonderlandContext;
+
+import org.jdesktop.wonderland.server.cell.CellMO;
+import org.jdesktop.wonderland.server.cell.CellComponentMO;
+import org.jdesktop.wonderland.server.cell.ChannelComponentMO;
+import org.jdesktop.wonderland.server.cell.ChannelComponentMO.ComponentMessageReceiver;
+
+import org.jdesktop.wonderland.server.comms.WonderlandClientSender;
+
+import org.jdesktop.wonderland.modules.audiomanager.common.AudioTreatmentComponentSetup;
+
+import org.jdesktop.wonderland.modules.audiomanager.common.messages.AudioTreatmentMessage;
+
+import com.sun.voip.client.connector.CallStatus;
+import com.sun.voip.client.connector.CallStatusListener;
+
+import com.sun.voip.CallParticipant;
+
+import com.sun.mpk20.voicelib.app.ManagedCallStatusListener;
+import com.sun.mpk20.voicelib.app.Treatment;
+import com.sun.mpk20.voicelib.app.TreatmentGroup;
+import com.sun.mpk20.voicelib.app.TreatmentSetup;
+import com.sun.mpk20.voicelib.app.VoiceManager;
+
+/**
+ *
+ * @author jprovino
+ */
+public class AudioTreatmentComponentMO extends CellComponentMO implements ManagedCallStatusListener {
+
+    private static final Logger logger =
+            Logger.getLogger(AudioTreatmentComponentMO.class.getName());
+
+    private ManagedReference<ChannelComponentMO> channelComponentRef = null;
+    
+    private String groupId;
+    private String[] treatments;
+    private double x;
+    private double y;
+    private double z;
+
+    /**
+     * Create a AudioTreatmentComponent for the given cell. The cell must already
+     * have a ChannelComponent otherwise this method will throw an IllegalStateException
+     * @param cell
+     */
+    public AudioTreatmentComponentMO(CellMO cell) {
+        super(cell);
+        
+        ChannelComponentMO channelComponent = (ChannelComponentMO) 
+	    cell.getComponent(ChannelComponentMO.class);
+
+        if (channelComponent == null) {
+            logger.warning("Cell does not have a ChannelComponent");
+	    return;
+	}
+
+        channelComponentRef = AppContext.getDataManager().createReference(channelComponent); 
+                
+        channelComponent.addMessageReceiver(AudioTreatmentMessage.class, new ComponentMessageReceiverImpl(this));
+    }
+    
+    @Override
+    public void setupCellComponent(CellComponentSetup setup) {
+	AudioTreatmentComponentSetup accs = (AudioTreatmentComponentSetup) setup;
+
+	treatments = accs.getTreatments();
+
+	groupId = accs.getGroupId();
+    }
+
+    @Override
+    public void setLive(boolean live) {
+	VoiceManager vm = AppContext.getManager(VoiceManager.class);
+
+	TreatmentGroup group = vm.createTreatmentGroup(groupId);
+
+	for (int i = 0; i < treatments.length; i++) {
+	    TreatmentSetup setup = new TreatmentSetup();
+
+	    setup.treatment = treatments[i];
+	    
+	    if (setup.treatment == null || setup.treatment.length() == 0) {
+		logger.warning("Invalid treatment '" + setup.treatment + "'");
+		continue;
+	    }
+
+	    Vector3f location = cellRef.get().getLocalTransform(null).getTranslation(null);
+
+	    setup.x = -location.getX();
+	    setup.y = location.getY();
+	    setup.z = location.getZ();
+
+	    logger.info("Starting treatment " + setup.treatment 
+		+ " at (" + setup.x + ":" + setup.y + ":" + setup.z + ")");
+
+	    try {
+	        group.addTreatment(vm.createTreatment(setup.treatment, setup));
+	    } catch (IOException e) {
+	        logger.warning("Unable to create treatment " + setup.treatment
+		    + e.getMessage());
+    	        return;
+	    }
+	}
+    }
+    
+    private static class ComponentMessageReceiverImpl implements ComponentMessageReceiver {
+
+        private ManagedReference<AudioTreatmentComponentMO> compRef;
+        
+        public ComponentMessageReceiverImpl(AudioTreatmentComponentMO comp) {
+            compRef = AppContext.getDataManager().createReference(comp);
+        }
+
+        public void messageReceived(WonderlandClientSender sender, ClientSession session, 
+		CellMessage message) {
+
+            AudioTreatmentMessage msg = (AudioTreatmentMessage) message;
+        }
+    }
+
+    public void callStatusChanged(CallStatus callStatus) {
+	String callId = callStatus.getCallId();
+
+	switch (callStatus.getCode()) {
+	case CallStatus.ESTABLISHED:
+	    break;
+
+	case CallStatus.TREATMENTDONE:
+	    break;
+	}
+
+    }
+
+}
