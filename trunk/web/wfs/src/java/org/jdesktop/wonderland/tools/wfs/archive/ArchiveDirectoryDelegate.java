71dd4749f78c9ae5d2b9651d1cfa8174637865d2
paulby <paulby@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-01-12 19:51:30 +0000

    Update legal header (was missing classpath exception)

--- trunk/web/wfs/src/java/org/jdesktop/wonderland/tools/wfs/archive/ArchiveDirectoryDelegate.java
+++ trunk/web/wfs/src/java/org/jdesktop/wonderland/tools/wfs/archive/ArchiveDirectoryDelegate.java
@@ -1,9 +1,7 @@
 /**
- * Project Looking Glass
+ * Project Wonderland
  *
- * $RCSfile: WFSArchiveCellDirectory.java,v $
- *
- * Copyright (c) 2004-2007, Sun Microsystems, Inc., All Rights Reserved
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
  *
  * Redistributions in source code form must reproduce the above
  * copyright and this condition.
@@ -13,11 +11,10 @@
  * except in compliance with the License. A copy of the License is
  * available at http://www.opensource.org/licenses/gpl-license.php.
  *
- * $Revision: 1.2.8.3 $
- * $Date: 2008/04/08 10:44:29 $
- * $State: Exp $
+ * Sun designates this particular file as subject to the "Classpath" 
+ * exception as provided by Sun in the License file that accompanied 
+ * this code.
  */
-
 package org.jdesktop.wonderland.tools.wfs.archive;
 
 import java.io.IOException;
