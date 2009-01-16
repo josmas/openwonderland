348b2cad79768fd7117bfdeadba791a69ea631d9
kaplanj <jonathankap@users.noreply.github.com>
2009-01-16 01:05:34 +0000

    Branch for experimenting with annotations

---
+++ annotation/modules/foundation/appbase/src/classes/org/jdesktop/wonderland/modules/appbase/common/AppCellServerState.java
@@ -0,0 +1,50 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath" 
+ * exception as provided by Sun in the License file that accompanied 
+ * this code.
+ */
+package org.jdesktop.wonderland.modules.appbase.server;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import org.jdesktop.wonderland.common.cell.state.CellServerState;
+
+/**
+ * The WFS server state class for AppCellMO.
+ * 
+ * @author deronj
+ */
+@XmlRootElement(name="app-cell")
+public class AppCellServerState extends CellServerState implements Serializable {
+    
+    /** Default constructor */
+    public AppCellServerState() {}
+    
+    public String getServerClassName() {
+        return "org.jdesktop.wonderland.modules.appbase.server.AppCellMO";
+    }
+    
+    /**
+     * Returns a string representation of this class.
+     *
+     * @return The server state information as a string.
+     */
+    @Override
+    public String toString() {
+        return super.toString() + " [AppCellServerState]";
+    }
+}
