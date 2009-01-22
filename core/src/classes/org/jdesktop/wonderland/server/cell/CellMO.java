e043ee767013f87c69dc68f4088ff7228a9e412c
jprovino <jprovino@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-01-22 21:01:59 +0000

    handle null transformchangelisteners when removing a listener.
    Make a copy of the component list before calling each components setLive()
    method.  This allows components to add other components.

--- core/src/classes/org/jdesktop/wonderland/server/cell/CellMO.java
+++ core/src/classes/org/jdesktop/wonderland/server/cell/CellMO.java
@@ -384,9 +384,11 @@ public abstract class CellMO implements ManagedObject, Serializable {
 
 
         // Notify all components of new live state
-        Collection<ManagedReference<CellComponentMO>> compList = components.values();
-        for(ManagedReference<CellComponentMO> c : compList) {
-            c.get().setLive(live);
+	ManagedReference<CellComponentMO>[] compList = (ManagedReference<CellComponentMO>[]) 
+	    components.values().toArray(new ManagedReference[0]);
+
+	for (int i = 0; i < compList.length; i++) {
+            compList[i].get().setLive(live);
         }
         
         for(ManagedReference<CellMO> ref : getAllChildrenRefs()) {
@@ -720,6 +722,8 @@ public abstract class CellMO implements ManagedObject, Serializable {
      * @param listener to be removed
      */
     public void removeTransformChangeListener(TransformChangeListenerSrv listener) {
+        if (transformChangeListeners==null)
+	    return;
         transformChangeListeners.remove(listener);
         if (isLive())
             UniverseManagerFactory.getUniverseManager().removeTransformChangeListener(this, listener);
