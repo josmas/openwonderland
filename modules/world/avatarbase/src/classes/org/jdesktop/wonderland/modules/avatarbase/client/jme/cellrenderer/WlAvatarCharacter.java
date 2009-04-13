b80595ac6d522654538b024fa38dffb41ee45a6b
nsimpson <nsimpson@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-04-13 17:35:41 +0000

    Key bindings for humans: arrow keys move avatar as they did in 0.4. Page up and page down move avatar up and down, like 0.4, but also for people with the cool Apple aluminum keyboards that lack numpad keys (!)

--- modules/world/avatarbase/src/classes/org/jdesktop/wonderland/modules/avatarbase/client/jme/cellrenderer/WlAvatarCharacter.java
+++ modules/world/avatarbase/src/classes/org/jdesktop/wonderland/modules/avatarbase/client/jme/cellrenderer/WlAvatarCharacter.java
@@ -87,14 +87,20 @@ public class WlAvatarCharacter extends Avatar {
     {
         m_keyBindings.put(KeyEvent.VK_SHIFT,        TriggerNames.Movement_Modifier.ordinal());
         m_keyBindings.put(KeyEvent.VK_A,            TriggerNames.Move_Left.ordinal());
+        m_keyBindings.put(KeyEvent.VK_LEFT,         TriggerNames.Move_Left.ordinal());
         m_keyBindings.put(KeyEvent.VK_D,            TriggerNames.Move_Right.ordinal());
+        m_keyBindings.put(KeyEvent.VK_RIGHT,        TriggerNames.Move_Right.ordinal());
         m_keyBindings.put(KeyEvent.VK_W,            TriggerNames.Move_Forward.ordinal());
+        m_keyBindings.put(KeyEvent.VK_UP,           TriggerNames.Move_Forward.ordinal());
         m_keyBindings.put(KeyEvent.VK_S,            TriggerNames.Move_Back.ordinal());
-//        m_keyBindings.put(KeyEvent.VK_CONTROL,      TriggerNames.MiscAction.ordinal());
+        m_keyBindings.put(KeyEvent.VK_DOWN,         TriggerNames.Move_Back.ordinal());
+        //        m_keyBindings.put(KeyEvent.VK_CONTROL,      TriggerNames.MiscAction.ordinal());
         m_keyBindings.put(KeyEvent.VK_ENTER,        TriggerNames.ToggleSteering.ordinal());
         m_keyBindings.put(KeyEvent.VK_HOME,         TriggerNames.GoSit.ordinal());
         m_keyBindings.put(KeyEvent.VK_ADD,          TriggerNames.Move_Down.ordinal());
+        m_keyBindings.put(KeyEvent.VK_PAGE_UP,      TriggerNames.Move_Down.ordinal());
         m_keyBindings.put(KeyEvent.VK_SUBTRACT,     TriggerNames.Move_Up.ordinal());
+        m_keyBindings.put(KeyEvent.VK_PAGE_DOWN,    TriggerNames.Move_Up.ordinal());
         m_keyBindings.put(KeyEvent.VK_COMMA,        TriggerNames.Reverse.ordinal());
         m_keyBindings.put(KeyEvent.VK_PERIOD,       TriggerNames.NextAction.ordinal());
         m_keyBindings.put(KeyEvent.VK_1,            TriggerNames.GoTo1.ordinal());
