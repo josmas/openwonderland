f796b86580bce37600394d031c8df8491acbdc31
jslott <jslott@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-04-15 16:07:19 +0000

    Text chat support for Wonderland. Supports both "global" chat (everyone) and person-to-person text chat. Added a "Text Chat" menu under "Tools" that has two checkbox items, to display the "global" chat window and a window to display a list of users to launch chat sessions.
    Added new connection type, handled on the server with TextChatConnectionHandler and registered via a server
    plugin. The client connection is represented by TextChatConnection.
    
    There is a single message: TextChatMessage that contains the String message, and the from/to. (For a "global"
    message, the "to" is an empty string).
    
    Registers a component on avatars for a context menu to launch a chat with that user. This does not yet work,
    since it seems the context menu does not get events for avatars.

---
+++ trunk/modules/world/text-chat/src/classes/org/jdesktop/wonderland/modules/textchat/client/ChatManager.java
@@ -0,0 +1,301 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath" 
+ * exception as provided by Sun in the License file that accompanied 
+ * this code.
+ */
+package org.jdesktop.wonderland.modules.textchat.client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import org.jdesktop.wonderland.client.comms.ConnectionFailureException;
+import org.jdesktop.wonderland.client.comms.WonderlandSession;
+import org.jdesktop.wonderland.client.jme.JmeClientMain;
+import org.jdesktop.wonderland.client.login.ServerSessionManager;
+import org.jdesktop.wonderland.client.login.SessionLifecycleListener;
+import org.jdesktop.wonderland.modules.textchat.client.TextChatConnection.TextChatListener;
+
+/**
+ * Manages all of the Text Chat windows for the client.
+ * 
+ * @author Jordan Slott <jslott@dev.java.net>
+ */
+public class ChatManager implements TextChatListener {
+
+    private static Logger logger = Logger.getLogger(ChatManager.class.getName());
+    private WeakReference<ChatUserListJFrame> userListFrameRef = null;
+    private Map<String, WeakReference<TextChatJFrame>> textChatFrameRefMap = null;
+    private JCheckBoxMenuItem textChatMenuItem = null;
+    private JCheckBoxMenuItem userListMenuItem = null;
+    private TextChatConnection textChatConnection = null;
+    private String localUserName = null;
+
+    /** Constructor */
+    public ChatManager(final ServerSessionManager loginInfo) {
+        textChatFrameRefMap = new HashMap();
+
+        // Create a new Chat menu underneath the "Tools" menu
+        JMenu menu = new JMenu("Text Chat");
+
+        // First create the text chat frame and keep a weak reference to it so
+        // that it gets garbage collected
+        final TextChatJFrame textChatJFrame = new TextChatJFrame();
+        final WeakReference<TextChatJFrame> frameRef = new WeakReference(textChatJFrame);
+        textChatFrameRefMap.put("", frameRef);
+
+        // Add the global text chat frame to the menu item. Listen for when it
+        // is selected or de-selected and show/hide the frame as appropriate.
+        textChatMenuItem = new JCheckBoxMenuItem("Text Chat (Everyone)");
+        textChatMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFrame textChatJFrame = frameRef.get();
+                textChatJFrame.setVisible(textChatMenuItem.isSelected());
+            }
+        });
+        textChatMenuItem.setEnabled(false);
+        menu.add(textChatMenuItem);
+
+        // Listen for when the text chat frame is closed by itself and
+        // uncheck the checkbox item in the menu
+        textChatJFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                textChatMenuItem.setSelected(false);
+            }
+        });
+
+        // Add the user list frame to the menu item. Listen for when it is
+        // selected or de-selected and show/hide the frame as appropriate.
+        userListMenuItem = new JCheckBoxMenuItem("Text Chat User List", false);
+        userListMenuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFrame userListJFrame = userListFrameRef.get();
+                userListJFrame.setVisible(userListMenuItem.isSelected());
+            }
+        });
+        userListMenuItem.setEnabled(false);
+        menu.add(userListMenuItem);
+
+        // Add the item to the tools menu and make the Asset Meter visible
+        // by default initially.
+        JmeClientMain.getFrame().addToToolMenu(menu);
+
+        // Wait for a primary session to become active. When it does, then
+        // we enable the menu items and set the primary sessions on their
+        // objects.
+        loginInfo.addLifecycleListener(new SessionLifecycleListener() {
+            public void sessionCreated(WonderlandSession session) {
+                // Do nothing for now
+            }
+
+            public void primarySession(WonderlandSession session) {
+                setPrimarySession(session);
+            }
+        });
+
+        // XXX Check if we already have primary session, should be handled
+        // by addLifecycleListener
+        if (loginInfo.getPrimarySession() != null) {
+            setPrimarySession(loginInfo.getPrimarySession());
+        }
+    }
+
+    /**
+     * Sets the primary session, when it is made the primary session. This
+     * turns on everything: enables the menu items, displays the global chat
+     * dialog.
+     */
+    private void setPrimarySession(WonderlandSession session) {
+        // Capture the local user name for later use
+        localUserName = session.getUserID().getUsername();
+        
+        // Create a new custom connection to receive text chats. Register a
+        // listener that handles new text messages. Will display them in the
+        // window.
+        textChatConnection = new TextChatConnection();
+        textChatConnection.addTextChatListener(this);
+
+        // Open the text chat connection. If unsuccessful, then log an error
+        // and return.
+        try {
+            textChatConnection.connect(session);
+        } catch (ConnectionFailureException excp) {
+            logger.log(Level.WARNING, "Unable to establish a connection to " +
+                    "the chat connection.", excp);
+            return;
+        }
+
+        // Create the user list frame and keep a weak reference to it so that it
+        // gets garbage collected
+        final ChatUserListJFrame userListJFrame =
+                new ChatUserListJFrame(session.getUserID(), this);
+        userListFrameRef = new WeakReference(userListJFrame);
+
+        // Listen for when the text chat frame is closed by itself and uncheck
+        // the checkbox item in the menu
+        userListJFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                userListMenuItem.setSelected(false);
+            }
+        });
+
+        // Otherwise, enable all of the GUI elements. First enable the user list
+        // frame by setting its session
+        userListJFrame.setPrimarySession(session);
+        userListMenuItem.setEnabled(true);
+
+        // Next, for the global chat, set its information and make it visible
+        // initially.
+        TextChatJFrame textChatJFrame = textChatFrameRefMap.get("").get();
+        textChatJFrame.setActive(textChatConnection, localUserName, "");
+        textChatMenuItem.setEnabled(true);
+        textChatMenuItem.setSelected(true);
+        textChatJFrame.setVisible(true);
+    }
+
+    /**
+     * Creates a new text chat window, given the remote participants user name
+     * and displays it.
+     *
+     * @param remoteUser The remote participants user name
+     */
+    public void startChat(String remoteUser) {
+        // Do all of this synchronized. This makes sure that multiple text chat
+        // window aren't create if a local user clicks to create a new text
+        // chat and a message comes in for that remote user.
+        synchronized (textChatFrameRefMap) {
+            // Check to see if the text chat window already exists. If so, then
+            // we do nothing and return.
+            WeakReference<TextChatJFrame> ref = textChatFrameRefMap.get(remoteUser);
+            if (ref != null) {
+                return;
+            }
+
+            // Otherwise, create the frame, add it to the map, and display
+            TextChatJFrame frame = new TextChatJFrame();
+            final String userKey = remoteUser;
+            frame.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    // Remove from the map which will let it garbage collect
+                    synchronized (textChatFrameRefMap) {
+                        e.getWindow().dispose();
+                        textChatFrameRefMap.remove(userKey);
+                    }
+                }
+            });
+            textChatFrameRefMap.put(remoteUser, new WeakReference(frame));
+            frame.setActive(textChatConnection, localUserName, remoteUser);
+            frame.setVisible(true);
+        }
+    }
+
+    /**
+     * Deactivates the text chat given the remote user's name, if such a frame
+     * exists. Displays a message in the window and turns off its GUI.
+     *
+     * @param remoteUser The remote participants user name
+     */
+    public void deactivateChat(String remoteUser) {
+        // Do all of this synchronized, so that we do not interfere with the
+        // code to create chats
+        synchronized (textChatFrameRefMap) {
+            // Check to see if the text chat window exists. If not, then do
+            // nothing.
+            WeakReference<TextChatJFrame> ref = textChatFrameRefMap.get(remoteUser);
+            if (ref == null) {
+                return;
+            }
+            TextChatJFrame frame = ref.get();
+            frame.deactivate();
+        }
+    }
+
+    /**
+     * Re-activates the text chat given the remote user's name, if such a frame
+     * exists. Displays a message in the window and turns on its GUI.
+     */
+    public void reactivateChat(String remoteUser) {
+        // Do all of this synchronized, so that we do not interfere with the
+        // code to create chats
+        synchronized (textChatFrameRefMap) {
+            // Check to see if the text chat window exists. If not, then do
+            // nothing.
+            WeakReference<TextChatJFrame> ref = textChatFrameRefMap.get(remoteUser);
+            if (ref == null) {
+                return;
+            }
+            TextChatJFrame frame = ref.get();
+            frame.reactivate();
+        }
+    }
+
+    /**
+     * @inheritDoc()
+     */
+    public void textMessage(String message, String fromUser, String toUser) {
+        // Fetch the frame associated with the user. If the "to" user is an
+        // empty string, then this is a "global" message and we fetch its
+        // frame. It should exist. We always add the message, no matter whether
+        // the frame is visible or not.
+        if (toUser == null || toUser.equals("") == true) {
+            TextChatJFrame frame = textChatFrameRefMap.get("").get();
+            frame.appendTextMessage(message, fromUser);
+            return;
+        }
+
+        // Otherwise, the "toUser" is for this specific user. We fetch the
+        // frame associated with the "from" user. If it exists (which also
+        // means it is visible, then add the message.
+        synchronized (textChatFrameRefMap) {
+            WeakReference<TextChatJFrame> ref = textChatFrameRefMap.get(fromUser);
+            if (ref != null) {
+                TextChatJFrame frame = ref.get();
+                frame.appendTextMessage(message, fromUser);
+                return;
+            }
+
+            // Finally, we reached here when we have a message from a specific
+            // user, but the frame does not exist, and is not visible. So we
+            // create it and add to the map and display it.
+            TextChatJFrame frame = new TextChatJFrame();
+            final String userKey = fromUser;
+            frame.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    // Remove from the map which will let it garbage collect
+                    synchronized (textChatFrameRefMap) {
+                        e.getWindow().dispose();
+                        textChatFrameRefMap.remove(userKey);
+                    }
+                }
+            });
+            textChatFrameRefMap.put(fromUser, new WeakReference(frame));
+            frame.setActive(textChatConnection, toUser, fromUser);
+            frame.setVisible(true);
+            frame.appendTextMessage(message, fromUser);
+        }
+    }
+}
---
+++ trunk/modules/world/text-chat/src/classes/org/jdesktop/wonderland/modules/textchat/client/ChatUserListJFrame.java
@@ -0,0 +1,221 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath"
+ * exception as provided by Sun in the License file that accompanied
+ * this code.
+ */
+package org.jdesktop.wonderland.modules.textchat.client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+import javax.swing.DefaultListModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import org.jdesktop.wonderland.client.comms.WonderlandSession;
+import org.jdesktop.wonderland.common.auth.WonderlandIdentity;
+import org.jdesktop.wonderland.modules.presencemanager.client.PresenceManager;
+import org.jdesktop.wonderland.modules.presencemanager.client.PresenceManagerFactory;
+import org.jdesktop.wonderland.modules.presencemanager.client.PresenceManagerListener;
+import org.jdesktop.wonderland.modules.presencemanager.common.PresenceInfo;
+
+/**
+ * A list of users that someone can start a chat with.
+ *
+ * @author Jordan Slott <jslott@dev.java.net>
+ */
+public class ChatUserListJFrame extends javax.swing.JFrame {
+
+    private Logger logger = Logger.getLogger(ChatUserListJFrame.class.getName());
+    private PresenceManager presenceManager = null;
+    private ChatManager chatManager = null;
+    final private DefaultListModel listModel;
+    private Map<String, WonderlandIdentity> userMap = null;
+    private WonderlandIdentity localUserIdentity = null;
+
+    /** Creates new form UserListJFrame */
+    public ChatUserListJFrame(WonderlandIdentity id, ChatManager manager) {
+        userMap = Collections.synchronizedMap(new HashMap());
+        chatManager = manager;
+        localUserIdentity = id;
+        initComponents();
+
+        // Set the list model on the list so we can add to it
+        userList.setModel(listModel = new DefaultListModel());
+
+        // Enable the "Start Chat" button only if an item is selected
+        startChatButton.setEnabled(false);
+        userList.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+                startChatButton.setEnabled(userList.getSelectedIndex() != -1);
+            }
+        });
+
+        // Upon a double-click of the user list, activate the start button
+        userList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    int index = userList.locationToIndex(e.getPoint());
+                    userList.setSelectedIndex(index);
+                    startChatButton.doClick();
+                }
+            }
+        });
+
+        // When the "Start Chat" button is selected, tell the chat manager to
+        // begin a new chat
+        startChatButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String selectedUser = (String)userList.getSelectedValue();
+                if (selectedUser == null) {
+                    logger.warning("No user selected on chat window");
+                    return;
+                }
+
+                WonderlandIdentity id = userMap.get(selectedUser);
+                if (id == null) {
+                    logger.warning("No ID found for user " + selectedUser);
+                    return;
+                }
+                String remoteUser = id.getUsername();
+                chatManager.startChat(remoteUser);
+            }
+        });
+    }
+
+    /**
+     * Sets the primary session for the text chat. This turns on everything.
+     *
+     * @param session The primary Wonderland session
+     */
+    public void setPrimarySession(WonderlandSession session) {
+        // Create the new presence manager with the given session and initialize
+        // the list of users
+        presenceManager = PresenceManagerFactory.getPresenceManager(session);
+        initUserList();
+
+         // Listen for changes in the users, and update the list as appropriate
+        presenceManager.addPresenceManagerListener(new PresenceManagerListener() {
+            public void userAdded(PresenceInfo pInfo) {
+                // Add the user if it does not already exist and it is not the
+                // same as this user.
+                String displayName = getDisplayName(pInfo.userID);
+                if (userMap.containsKey(displayName) == false) {
+                    if (pInfo.userID.equals(localUserIdentity) == false) {
+                        userMap.put(displayName, pInfo.userID);
+                        listModel.addElement(displayName);
+                    }
+                }
+
+                // Check to see if a frame already exists from a previous
+                // session. If so, then reactivate it.
+                chatManager.reactivateChat(pInfo.userID.getUsername());
+            }
+
+            public void userRemoved(PresenceInfo pInfo) {
+                // Remove the user if it does exist and it is not the same as
+                // this user.
+                String displayName = getDisplayName(pInfo.userID);
+                if (userMap.containsKey(displayName) == true) {
+                    if (pInfo.userID.equals(localUserIdentity) == false) {
+                        userMap.remove(displayName);
+                        listModel.removeElement(displayName);
+                    }
+                }
+
+                // Ask the Chat Manager if it has a frame corresponding to the
+                // user chat. If so, then it de-activates it.
+                chatManager.deactivateChat(pInfo.userID.getUsername());
+            }
+        });
+    }
+
+    /**
+     * Initializes the user list to all users except the local one.
+     */
+    private void initUserList() {
+        for (WonderlandIdentity id : presenceManager.getAllUsers()) {
+            if (id.equals(localUserIdentity) == false) {
+                String displayName = getDisplayName(id);
+                userMap.put(displayName, id);
+                listModel.addElement(displayName);
+            }
+        }
+    }
+
+    /**
+     * Given the user's presence info, returns its display name in the list
+     */
+    private String getDisplayName(WonderlandIdentity userID) {
+        String fullName = userID.getFullName();
+        String userName = userID.getUsername();
+        String displayName = fullName + " (" + userName + ")";
+        return displayName;
+    }
+
+    /** This method is called from within the constructor to
+     * initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is
+     * always regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        userListScrollPane = new javax.swing.JScrollPane();
+        userList = new javax.swing.JList();
+        startChatButton = new javax.swing.JButton();
+
+        setTitle("Chat User List");
+        getContentPane().setLayout(new java.awt.GridBagLayout());
+
+        userListScrollPane.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        userListScrollPane.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        userList.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        userList.setVisibleRowCount(15);
+        userListScrollPane.setViewportView(userList);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridheight = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        getContentPane().add(userListScrollPane, gridBagConstraints);
+
+        startChatButton.setText("Start Chat");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(0, 5, 5, 0);
+        getContentPane().add(startChatButton, gridBagConstraints);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton startChatButton;
+    private javax.swing.JList userList;
+    private javax.swing.JScrollPane userListScrollPane;
+    // End of variables declaration//GEN-END:variables
+}
