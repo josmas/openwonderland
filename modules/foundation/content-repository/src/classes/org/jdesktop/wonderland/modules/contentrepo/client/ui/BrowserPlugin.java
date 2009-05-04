278c2905be8ddd1d6036dbefdbbe39c6a028e65d
nsimpson <nsimpson@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-05-04 21:37:32 +0000

    Fix bug #137: Remove multiple content browsers from the Tools menu - commented out original content browser

--- modules/foundation/content-repository/src/classes/org/jdesktop/wonderland/modules/contentrepo/client/ui/BrowserPlugin.java
+++ modules/foundation/content-repository/src/classes/org/jdesktop/wonderland/modules/contentrepo/client/ui/BrowserPlugin.java
@@ -24,10 +24,7 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.AbstractAction;
-import javax.swing.Action;
 import javax.swing.JMenuItem;
-import javax.swing.SwingUtilities;
 import org.jdesktop.wonderland.client.BaseClientPlugin;
 import org.jdesktop.wonderland.client.cell.registry.CellRegistry;
 import org.jdesktop.wonderland.client.cell.registry.spi.CellFactorySPI;
@@ -54,33 +51,33 @@ public class BrowserPlugin extends BaseClientPlugin {
     private BrowserFrame frame;
     private WeakReference<ContentBrowserJDialog> browserDialogRef = null;
 
-    private JMenuItem oldBrowserItem;
+    //private JMenuItem oldBrowserItem;
     private JMenuItem newBrowserItem;
     private ContentBrowserJDialog defaultBrowser;
 
     @Override
     public void initialize(final ServerSessionManager loginInfo) {
-        Action launchAction = new AbstractAction("Content Browser") {
-            public synchronized void actionPerformed(ActionEvent e) {
-                if (frame == null) {
-                    ContentRepositoryRegistry registry = ContentRepositoryRegistry.getInstance();
-                    ContentRepository repo = registry.getRepository(loginInfo);
-                    frame = new BrowserFrame(repo);
-                }
-
-                SwingUtilities.invokeLater(new Runnable() {
-                    public void run() {
-                        frame.setVisible(true);
-                    }
-                });
-            }
-        };
-        oldBrowserItem = new JMenuItem(launchAction);
+//        Action launchAction = new AbstractAction("Content Browser") {
+//            public synchronized void actionPerformed(ActionEvent e) {
+//                if (frame == null) {
+//                    ContentRepositoryRegistry registry = ContentRepositoryRegistry.getInstance();
+//                    ContentRepository repo = registry.getRepository(loginInfo);
+//                    frame = new BrowserFrame(repo);
+//                }
+//
+//                SwingUtilities.invokeLater(new Runnable() {
+//                    public void run() {
+//                        frame.setVisible(true);
+//                    }
+//                });
+//            }
+//        };
+//        oldBrowserItem = new JMenuItem(launchAction);
 
 
         // Add the Palette menu and the Cell submenu and dialog that lets users
         // create new cells.
-        newBrowserItem = new JMenuItem("Content Browser (New)");
+        newBrowserItem = new JMenuItem("Content Browser");
         newBrowserItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 ContentBrowserJDialog contentBrowserFrame;
@@ -150,7 +147,7 @@ public class BrowserPlugin extends BaseClientPlugin {
         manager.setDefaultContentBrowser(defaultBrowser);
 
         // add menu items
-        JmeClientMain.getFrame().addToToolsMenu(oldBrowserItem, 5);
+        //JmeClientMain.getFrame().addToToolsMenu(oldBrowserItem, 5);
         JmeClientMain.getFrame().addToToolsMenu(newBrowserItem, 6);
     } 
     
@@ -164,7 +161,7 @@ public class BrowserPlugin extends BaseClientPlugin {
         }
 
         // remove menu items
-        JmeClientMain.getFrame().removeFromToolsMenu(oldBrowserItem);
+        //JmeClientMain.getFrame().removeFromToolsMenu(oldBrowserItem);
         JmeClientMain.getFrame().removeFromToolsMenu(newBrowserItem);
     }
 }
