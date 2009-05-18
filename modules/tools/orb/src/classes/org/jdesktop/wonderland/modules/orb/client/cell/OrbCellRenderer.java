f593586c22f9c61069266b0dd188356bd1905af5
jprovino <jprovino@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-05-18 16:27:44 +0000

    setSwingSafe(true) and get rid of the glContext stuff.

--- modules/tools/orb/src/classes/org/jdesktop/wonderland/modules/orb/client/cell/OrbCellRenderer.java
+++ modules/tools/orb/src/classes/org/jdesktop/wonderland/modules/orb/client/cell/OrbCellRenderer.java
@@ -42,7 +42,6 @@ import org.jdesktop.wonderland.client.input.EventClassListener;
 import org.jdesktop.wonderland.client.jme.ClientContextJME;
 import org.jdesktop.wonderland.client.jme.cellrenderer.BasicRenderer;
 import org.jdesktop.wonderland.common.cell.CellTransform;
-import javax.media.opengl.GLContext;
 
 import java.lang.reflect.Method;
 
@@ -216,60 +215,23 @@ public class OrbCellRenderer extends BasicRenderer {
 
     class MyMouseListener extends EventClassListener {
 
+	public MyMouseListener() {
+	    setSwingSafe(true);
+	}
+
 	public Class[] eventClassesToConsume () {
 	    return new Class[] { MouseEvent3D.class };
 	}
 
 	public void commitEvent (Event event) {
 	    if (event instanceof MouseButtonEvent3D) {
-		// Linux-specific workaround: On Linux JOGL holds the SunToolkit AWT lock in mtgame commit methods.
-		// In order to avoid deadlock with any threads which are already holding the AWT lock and which
-		// want to acquire the lock on the dirty rectangle so they can draw (e.g Embedded Swing threads)
-		// we need to temporarily release the AWT lock before we lock the dirty rectangle and then reacquire
-		// the AWT lock afterward.
-		GLContext glContext = null;
-		if (isAWTLockHeldByCurrentThreadMethod != null) {
-                    try {
-                        Boolean ret = (Boolean) isAWTLockHeldByCurrentThreadMethod.invoke(null);
-                        if (ret.booleanValue()) {
-                            glContext = GLContext.getCurrent();
-                            glContext.release();
-                        }
-                    } catch (Exception ex) {}
-                }
-
-		try {
-		    MouseButtonEvent3D buttonEvent = (MouseButtonEvent3D) event;
-		    if (buttonEvent.isPressed()) {
-			logger.info("Orb Selected");
-		        ((OrbCell) cell).orbSelected();
-		    }
-		    return;
-		} finally {
-		    // Linux-specific workaround: Reacquire the lock if necessary.
-		    if (glContext != null) {
-			glContext.makeCurrent();
-                    }
+		MouseButtonEvent3D buttonEvent = (MouseButtonEvent3D) event;
+		if (buttonEvent.isPressed()) {
+		    logger.info("Orb Selected");
+		    ((OrbCell) cell).orbSelected();
 		}
 	    } 
 	}
-
-    }
-
-    // We need to call this method reflectively because it isn't available in Java 5
-    // BTW: we don't support Java 5 on Linux, so this is okay.
-    private static boolean isLinux = System.getProperty("os.name").equals("Linux");
-    private static Method isAWTLockHeldByCurrentThreadMethod;
-    static {
-        if (isLinux) {
-            try {
-                Class awtToolkitClass = Class.forName("sun.awt.SunToolkit");
-                isAWTLockHeldByCurrentThreadMethod =
-                    awtToolkitClass.getMethod("isAWTLockHeldByCurrentThread");
-            } catch (ClassNotFoundException ex) {
-            } catch (NoSuchMethodException ex) {
-            }
-        }
     }
 
 }
