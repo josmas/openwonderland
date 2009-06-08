5e77b7372c0fc2da6ad84a084788794b2ec8e2b9
paulby <paulby@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-06-08 22:45:50 +0000

    Missed a file

--- modules/foundation/swingwhiteboard/src/classes/org/jdesktop/wonderland/modules/swingwhiteboard/client/SwingWhiteboardComponent.java
+++ modules/foundation/swingwhiteboard/src/classes/org/jdesktop/wonderland/modules/swingwhiteboard/client/SwingWhiteboardComponent.java
@@ -38,18 +38,14 @@ import org.jdesktop.wonderland.modules.swingwhiteboard.common.WhiteboardCellMess
  */
 @ExperimentalAPI
 public class SwingWhiteboardComponent extends CellComponent {
-    
-    private static Logger logger = Logger.getLogger(SwingWhiteboardComponent.class.getName());
 
+    private static Logger logger = Logger.getLogger(SwingWhiteboardComponent.class.getName());
     /** An object which sends drawing messages to the server cell */
     private BufferedCompoundMessageSender bufferedSender;
-
     /** The cell channel communications component. */
     private ChannelComponent channelComp;
-    
     /** The message receiver of this class */
     private ChannelComponent.ComponentMessageReceiver msgReceiver;
-
     /** The cell to which this component belongs. */
     private SwingWhiteboardCell cell;
 
@@ -58,69 +54,71 @@ public class SwingWhiteboardComponent extends CellComponent {
      * @param cell The cell to which this component belongs.
      * @throws IllegalStateException If the cell does not already have a ChannelComponent IllegalStateException will be thrown.
      */
-    public SwingWhiteboardComponent (Cell cell) {
+    public SwingWhiteboardComponent(Cell cell) {
         super(cell);
         this.cell = (SwingWhiteboardCell) cell;
     }
-    
+
     /**
      * React to cell status changes by listening to 
      * @param status The current status of this cell.
      */
-    @Override
-    public void setStatus (CellStatus status) {
-         switch(status) {
+    protected void setStatus(CellStatus status, boolean increasing) {
+        super.setStatus(status, increasing);
+        switch (status) {
 
-             case INACTIVE: {
+            case ACTIVE: {
+                if (increasing) {
+                    channelComp = cell.getComponent(ChannelComponent.class);
+                    if (channelComp == null) {
+                        throw new IllegalStateException("Cell does not have a ChannelComponent");
+                    }
+                    bufferedSender = new BufferedCompoundMessageSender(channelComp);
 
-		 channelComp = cell.getComponent(ChannelComponent.class);
-		 if (channelComp == null) {
-		     throw new IllegalStateException("Cell does not have a ChannelComponent");
-		 }
-		 bufferedSender = new BufferedCompoundMessageSender(channelComp);
+                    if (msgReceiver == null) {
+                        msgReceiver = new ChannelComponent.ComponentMessageReceiver() {
 
-                 if (msgReceiver==null) {
-                    msgReceiver = new ChannelComponent.ComponentMessageReceiver() {
-                        public void messageReceived(CellMessage message) {
-			    // All messages sent over the connection are of this type
-                            WhiteboardCompoundCellMessage msg = (WhiteboardCompoundCellMessage)message;
+                            public void messageReceived(CellMessage message) {
+                                // All messages sent over the connection are of this type
+                                WhiteboardCompoundCellMessage msg = (WhiteboardCompoundCellMessage) message;
 
-			    // Note: The new DarkStar doesn't support the exclusion of certain clients from
-			    // its message broadcasts. Therefore all messages that we send will be echoed
-			    // back to us! Therefore we must ignore the echoed messages.
-			    BigInteger msgClientID = msg.getClientID();
-			    if (msgClientID == null || 
-				!msgClientID.equals(cell.getClientID())) {
-				cell.processMessage(msg);
-			    }
-                        }
-                    };                    
-                    channelComp.addMessageReceiver(WhiteboardCompoundCellMessage.class, msgReceiver);
-                 }
+                                // Note: The new DarkStar doesn't support the exclusion of certain clients from
+                                // its message broadcasts. Therefore all messages that we send will be echoed
+                                // back to us! Therefore we must ignore the echoed messages.
+                                BigInteger msgClientID = msg.getClientID();
+                                if (msgClientID == null ||
+                                        !msgClientID.equals(cell.getClientID())) {
+                                    cell.processMessage(msg);
+                                }
+                            }
+                        };
+                        channelComp.addMessageReceiver(WhiteboardCompoundCellMessage.class, msgReceiver);
+                    }
 
-		 // Must do *after* registering the listener.
-		 sync();
+                    // Must do *after* registering the listener.
+                    sync();
 
-		 logger.warning("********* SwingWhiteboard cell initialization complete, cellID = " + cell.getCellID());
-		 break;
-             }
+//                logger.warning("********* SwingWhiteboard cell initialization complete, cellID = " + cell.getCellID());
+                }
+                break;
+            }
 
-            case DISK :
-                if (msgReceiver!=null) {
+            case DISK:
+                if (msgReceiver != null) {
                     channelComp.removeMessageReceiver(WhiteboardCompoundCellMessage.class);
                     msgReceiver = null;
                 }
-		channelComp = null;
-		bufferedSender = null;
+                channelComp = null;
+                bufferedSender = null;
                 break;
         }
     }
-    
+
     /**
      * Send a command to sync with the shared whiteboard data. The server will respond with
      * messages which provide the latest data.
      */
-    private void sync () {
+    private void sync() {
         WhiteboardCompoundCellMessage cmsg = new WhiteboardCompoundCellMessage(cell.getClientID(), cell.getCellID(),
                 WhiteboardAction.REQUEST_SYNC);
         channelComp.send(cmsg);
@@ -130,7 +128,7 @@ public class SwingWhiteboardComponent extends CellComponent {
      * Send a drawing command to the server.
      * @param msg The message to send.
      */
-    public void sendMessage (WhiteboardCellMessage msg) {
-	bufferedSender.enqueue(msg);
+    public void sendMessage(WhiteboardCellMessage msg) {
+        bufferedSender.enqueue(msg);
     }
 }
