33267cd57e12761dfeab178a9ffd6aa357cd763b
nsimpson <nsimpson@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-06-23 22:18:39 +0000

    Name tag improvements:
    - Name tags are now centered over the avatar rather than shifted left (noticeable with long names)
    - Name tags now scale correctly:
    
      To change name tag font size:
        Window > Users
        select a user, then press [Properties]
        use the font size spinners to change the size for yourself and other users
        you can also hide name tags
    
    - TextLabel2D now correctly scales and positions text

--- trunk/core/src/classes/org/jdesktop/wonderland/client/jme/utils/TextLabel2D.java
+++ trunk/core/src/classes/org/jdesktop/wonderland/client/jme/utils/TextLabel2D.java
@@ -1,7 +1,6 @@
 /*
  * Sample code form JME wiki, http://jmonkeyengine.com/wiki/doku.php?id=billboard_awt_label
  */
-
 package org.jdesktop.wonderland.client.jme.utils;
 
 import java.awt.Color;
@@ -34,6 +33,7 @@ import java.awt.font.FontRenderContext;
 import java.awt.font.TextLayout;
 
 public class TextLabel2D extends Node {
+
     private String text;
     private float blurIntensity = 0.1f;
     private int kernelSize = 5;
@@ -45,34 +45,30 @@ public class TextLabel2D extends Node {
     private int shadowOffsetY = 2;
     private Font font;
     private Font drawFont;
-    private float height=1f;
-
+    private float height = 1f;
     private FontRenderContext fontRenderContext = null;
     private Quad quad;
 
     public TextLabel2D(String text) {
-        this(text, new Color(1f,1f,1f), new Color(0f,0f,0f), 0.3f, false, null);
+        this(text, new Color(1f, 1f, 1f), new Color(0f, 0f, 0f), 0.3f, false, null);
     }
-    
-    public TextLabel2D(String text,
-                       Color foreground,
-                       Color background,
-                       float height,
-                       boolean billboard,
-                       Font font) {
+
+    public TextLabel2D(String text, Color foreground, Color background,
+            float height, boolean billboard, Font font) {
         super();
         this.text = text;
         this.foreground = foreground;
         this.background = background;
         this.height = height;
         updateKernel();
-        if (font==null)
+        if (font == null) {
             font = Font.decode("Sans PLAIN 40");
+        }
         setFont(font);
         attachChild(getBillboard());
     }
 
-    public void setFont(Font font){
+    public void setFont(Font font) {
         this.font = font;
         BufferedImage tmp0 = new BufferedImage(10, 10, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = (Graphics2D) tmp0.getGraphics();
@@ -93,84 +89,120 @@ public class TextLabel2D extends Node {
         tmpParent.attachChild(getQuad());
     }
 
-    public void setShadowOffsetX(int offsetPixelX){
+    public void setShadowOffsetX(int offsetPixelX) {
         shadowOffsetX = offsetPixelX;
     }
-    public void setShadowOffsetY(int offsetPixelY){
+
+    public void setShadowOffsetY(int offsetPixelY) {
         shadowOffsetY = offsetPixelY;
     }
-    public void setBlurSize(int kernelSize){
+
+    public void setBlurSize(int kernelSize) {
         this.kernelSize = kernelSize;
         updateKernel();
     }
-    public void setBlurStrength(float strength){
+
+    public void setBlurStrength(float strength) {
         this.blurIntensity = strength;
         updateKernel();
     }
-    public void setFontResolution(float fontResolution){
+
+    public void setFontResolution(float fontResolution) {
         this.fontResolution = fontResolution;
     }
 
     private void updateKernel() {
-        float[] kernel = new float[kernelSize*kernelSize];
+        float[] kernel = new float[kernelSize * kernelSize];
         Arrays.fill(kernel, blurIntensity);
         blur = new ConvolveOp(new Kernel(kernelSize, kernelSize, kernel));
     }
 
     /**
+     * Generate an image of the label
      *
      * @param scaleFactors is set to the factors needed to adjust texture coords
-     * to the next-power-of-two- sized resulting image
+     * to the next power-of-two-sized resulting image
      */
-    private BufferedImage getImage(Vector2f scaleFactors){
+    private BufferedImage getImage(Vector2f scaleFactors) {
 
+        // calculate the size of the label text rendered with the specified font
         TextLayout layout = new TextLayout(text, font, fontRenderContext);
         Rectangle2D b = layout.getBounds();
 
-        int actualX = (int)b.getWidth()+kernelSize+1+shadowOffsetX;
-        int actualY = (int)b.getHeight()+kernelSize+1+shadowOffsetY;
+        // calculate the width of the label with shadow and blur
+        int actualWidth = (int) (b.getWidth() + kernelSize + 1 + shadowOffsetX);
 
-        int desiredX = FastMath.nearestPowerOfTwo(actualX);
-        int desiredY = FastMath.nearestPowerOfTwo(actualY);
+        // calculate the maximum height of the text including the ascents and
+        // descents of the characters
+        int actualHeight = (int) (layout.getAscent() + layout.getDescent() + kernelSize + 1 + shadowOffsetY);
 
-        if(scaleFactors != null){
-            scaleFactors.x = (float)actualX/desiredX;
-            scaleFactors.y = (float)actualY/desiredY;
+        // determine the closest power of two bounding box
+        //
+        // NOTE: we scale the text height to fit the nearest power or two, and
+        // then scale the text width equally to maintain the correct aspect
+        // ratio:
+        int desiredHeight = FastMath.nearestPowerOfTwo(actualHeight);
+        int desiredWidth = FastMath.nearestPowerOfTwo((int) (((float) desiredHeight / (float) actualHeight) * actualWidth));
+
+        // set the scale factors for scaling the text to fit the nearest power
+        // of two bounding box:
+        if (scaleFactors != null) {
+            // scale the text vertically to fit the height
+            scaleFactors.y = (float) desiredHeight / actualHeight;
+            // scale the text an equal amount horizontally to maintain aspect ratio
+            scaleFactors.x = scaleFactors.y;
         }
 
-        BufferedImage tmp0 = new BufferedImage(desiredX, desiredY, BufferedImage.TYPE_INT_ARGB);
-
+        // create an image to render the text onto
+        BufferedImage tmp0 = new BufferedImage(desiredWidth, desiredHeight, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = (Graphics2D) tmp0.getGraphics();
+        g2d.setFont(drawFont);
+
+//        // draw debugging text alignment lines
+//        g2d.setColor(Color.YELLOW);
+//        g2d.drawLine(0, desiredHeight / 2, desiredWidth, desiredHeight / 2);
+//        g2d.drawLine(desiredWidth / 2, 0, desiredWidth / 2, desiredHeight);
+
+        // center the text on the label
+        int scaledWidth = (int) (actualWidth * scaleFactors.x);
+        int textX = desiredWidth / 2 - scaledWidth / 2;// + kernelSize / 2;
+        int textY = desiredHeight / 2;
+
+//        // draw debugging text left and right bounds lines
+//        g2d.setColor(Color.RED);
+//        g2d.drawLine(textX, 0, textX, desiredHeight);
+//        g2d.drawLine(desiredWidth / 2 + scaledWidth / 2, 0, desiredWidth / 2 + scaledWidth / 2, desiredHeight);
+
+        // draw the shadow of the text
         g2d.setFont(drawFont);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-
-        int textX = kernelSize/2;
-        int textY = g2d.getFontMetrics().getMaxAscent() - kernelSize/2;
-
         g2d.setColor(background);
         g2d.drawString(text, textX + shadowOffsetX, textY + shadowOffsetY);
 
+        // blur the text
         BufferedImage ret = blur.filter(tmp0, null);
 
+        // draw the blurred text over the shadow
         g2d = (Graphics2D) ret.getGraphics();
         g2d.setFont(drawFont);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-
         g2d.setColor(foreground);
         g2d.drawString(text, textX, textY);
 
         return ret;
     }
 
-    private Quad getQuad(){
+    private Quad getQuad() {
         Vector2f scales = new Vector2f();
         BufferedImage img = getImage(scales);
-        float w = img.getWidth() * scales.x;
-        float h = img.getHeight() * scales.y;
+
+        float w = img.getWidth();
+        float h = img.getHeight();
         float factor = height / h;
-        Quad ret = new Quad("textLabel2d", w * factor , h * factor);
+
+        Quad ret = new Quad("textLabel2d", w * factor, h * factor);
         TextureState ts = DisplaySystem.getDisplaySystem().getRenderer().createTextureState();
         Texture tex = TextureManager.loadTexture(img, MinificationFilter.BilinearNoMipMaps, MagnificationFilter.Bilinear, true);
 
@@ -205,8 +237,8 @@ public class TextLabel2D extends Node {
         this.quad = ret;
         return ret;
     }
-    
-    private BillboardNode getBillboard(){
+
+    private BillboardNode getBillboard() {
         BillboardNode bb = new BillboardNode("bb");
         bb.attachChild(getQuad());
         return bb;
