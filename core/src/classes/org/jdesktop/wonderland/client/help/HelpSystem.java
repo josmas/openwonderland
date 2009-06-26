460581f4b1d5d1401367cecec2ba154da647670f
jslott <jslott@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-06-26 22:07:40 +0000

    Fixed the Help menu system so that it properly behaves when clients reconnect to other servers.

--- core/src/classes/org/jdesktop/wonderland/client/help/HelpSystem.java
+++ core/src/classes/org/jdesktop/wonderland/client/help/HelpSystem.java
@@ -19,8 +19,14 @@ package org.jdesktop.wonderland.client.help;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.SwingUtilities;
+import org.jdesktop.wonderland.client.login.LoginManager;
+import org.jdesktop.wonderland.client.login.PrimaryServerListener;
+import org.jdesktop.wonderland.client.login.ServerSessionManager;
 import org.jdesktop.wonderland.common.help.HelpInfo;
 
 /**
@@ -28,14 +34,21 @@ import org.jdesktop.wonderland.common.help.HelpInfo;
  * 
  * @author Jordan Slott <jslott@dev.java.net>
  */
-public class HelpSystem {
-    
-    /* The root help menu */
+public class HelpSystem implements PrimaryServerListener {
+
+    private static Logger logger = Logger.getLogger(HelpSystem.class.getName());
     private JMenu helpMenu = null;
     
     /** Constructor */
     public HelpSystem() {
-        this.reload();
+        // Create the root menu. This will be available immediately so the
+        // main frame can add it right away. It won't get populated until the
+        // session becomes active.
+        helpMenu = new JMenu("Help");
+
+        // Listen for connections to the primary server. Whenever there is a
+        // new primary server, we want to refresh the Help menu
+        LoginManager.addPrimaryServerListener(this);
     }
     
     /**
@@ -44,61 +57,92 @@ public class HelpSystem {
      * @return The base JMenu help menu
      */
     public JMenu getHelpJMenu() {
-        return this.helpMenu;
+        return helpMenu;
     }
-    
+
     /**
-     * Builds the menu system from scratch
+     * {@inheritDoc}
      */
-    private void reload() {      
-        /* Fetch the menu structure, if it does not exist, return an empty menu */
-        HelpInfo helpInfo = HelpUtils.fetchHelpInfo();
+    public void primaryServer(ServerSessionManager server) {
+        reload(server);
+    }
+
+    /**
+     * Builds the menu system from scratch.
+     */
+    private synchronized void reload(ServerSessionManager manager) {
+
+        // Fetch the menu structure, if it does not exist, return and leave
+        // the menu empty. We need to clear out the menu structure in the AWT
+        // Event Thread.
+        HelpInfo helpInfo = HelpUtils.fetchHelpInfo(manager);
         if (helpInfo == null) {
-            this.helpMenu = new JMenu("Help");
+            logger.warning("Unable to fetch Help Info from server");
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    helpMenu.removeAll();
+                }
+            });
             return;
         }
         
-        /* Generate the help menu structure */
-        HelpInfo.HelpMenuEntry entries[] = helpInfo.getHelpEntries();
-        this.helpMenu = this.buildJMenu("Help", entries);
+        // Generate the new menu structure. We need to update the menu structure
+        // in the AWT Event Thread.
+        final HelpInfo.HelpMenuEntry entries[] = helpInfo.getHelpEntries();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                helpMenu.removeAll();
+                buildJMenu(helpMenu, entries);
+            }
+        });
     }
     
     /**
-     * Builds and returns a JMenu representing a folder (and any sub-items)
+     * Builds the chilren beneath a JMenu representing a folder (and any
+     * sub-items) recursively.
      */
-    private JMenu buildJMenu(String name, HelpInfo.HelpMenuEntry[] entries) {
-        JMenu menu = new JMenu(name);
+    private void buildJMenu(JMenu menu, HelpInfo.HelpMenuEntry[] entries) {
+        // If there are no entries to add, then simply return.
         if (entries == null) {
-            return menu;
+            return;
         }
         
-        /* Loop through all of the entries and recursively create submenus */
+        // Loop through all of the entries and add them to the given menu.
+        // Recursively add children to sub-menus too.
         for (HelpInfo.HelpMenuEntry entry : entries) {
+            // If the entry is a "folder" (i.e. a sub-menu), create a new JMenu
+            // to put its children in, add the JMenu to the current menu and
+            // recursively add its children.
             if (entry instanceof HelpInfo.HelpMenuFolder) {
                 HelpInfo.HelpMenuFolder folder = (HelpInfo.HelpMenuFolder)entry;
-                    menu.add(this.buildJMenu(folder.name, folder.entries));
+                JMenu subMenu = new JMenu(folder.name);
+                menu.add(subMenu);
+                buildJMenu(subMenu, folder.entries);
             }
             else if (entry instanceof HelpInfo.HelpMenuItem) {
+                // If we are just adding a item, then create a new JMenuItem.
+                // We need to create a listener to act when the menu item is
+                // selected.
                 HelpInfo.HelpMenuItem item = (HelpInfo.HelpMenuItem)entry;
                 final String uri = item.helpURI;
                 JMenuItem menuItem = new JMenuItem(item.name);
                 menuItem.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent event) {
                         try {
-                            System.out.println("URL: " + uri);
                             WebBrowserLauncher.openURL(uri);
                         } catch (Exception excp) {
-                            excp.printStackTrace();
+                            logger.log(Level.WARNING, "Unable to launch URL " +
+                                    uri, excp);
                         }
                     }
                 });
                 menu.add(menuItem);
             }
             else if (entry instanceof HelpInfo.HelpMenuSeparator) {
+                // If we are adding a separator, then do so.
                 menu.addSeparator();
             }
         }
-        return menu;
     }
 }
 
