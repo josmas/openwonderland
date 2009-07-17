ba479875eeb488f5166ee7d6bdced403c7162afd
paulby <paulby@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-07-17 19:03:48 +0000

    Stub for loading MtGame config files Incomplete, uncomment code in MtgLoader.initialize to experiment. Does not support Deployment yet

---
+++ modules/world/mtgloader/build-tools/xjc.sh
@@ -0,0 +1,2 @@
+/System/Library/Frameworks/JavaVM.framework/Versions/Current6/Commands/xjc -d ../src/classes/ -b bindingsW.xjb http://schemas.opengis.net/kml/2.2.0/ogckml22.xsd
+#/System/Library/Frameworks/JavaVM.framework/Versions/Current6/Commands/xjc -d ../src/classes/ -b bindingsW.xjb -p org.jdesktop.wonderland.modules.kmzloader.client.kml_22 http://schemas.opengis.net/kml/2.2.0/ogckml22.xsd
