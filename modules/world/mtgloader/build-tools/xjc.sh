28ad7c5ed5b9021bdd45b6824f87e88f9fa1b595
paulby <paulby@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-07-17 19:04:11 +0000

    Remove unused

--- modules/world/mtgloader/build-tools/xjc.sh
+++
@@ -1,2 +0,0 @@
-/System/Library/Frameworks/JavaVM.framework/Versions/Current6/Commands/xjc -d ../src/classes/ -b bindingsW.xjb http://schemas.opengis.net/kml/2.2.0/ogckml22.xsd
-#/System/Library/Frameworks/JavaVM.framework/Versions/Current6/Commands/xjc -d ../src/classes/ -b bindingsW.xjb -p org.jdesktop.wonderland.modules.kmzloader.client.kml_22 http://schemas.opengis.net/kml/2.2.0/ogckml22.xsd
