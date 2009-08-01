42d7da49688a202f54cfc536bd281a37260c6a38
nsimpson <nsimpson@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-08-01 00:42:34 +0000

    HUD animation support: - HUD components now fade in and fade out when then appear and disappear in addition to responding to focus. - added support for better layout management (not operational yet.)

--- trunk/modules/foundation/hud/src/classes/org/jdesktop/wonderland/modules/hud/client/HUDAbsoluteLayoutManager.java
+++ trunk/modules/foundation/hud/src/classes/org/jdesktop/wonderland/modules/hud/client/HUDAbsoluteLayoutManager.java
@@ -19,6 +19,7 @@ package org.jdesktop.wonderland.modules.hud.client;
 
 import com.jme.math.Vector2f;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -39,7 +40,6 @@ public class HUDAbsoluteLayoutManager implements HUDLayoutManager {
     private static final Logger logger = Logger.getLogger(HUDAbsoluteLayoutManager.class.getName());
     // a mapping between HUD components and their views
     protected Map<HUDComponent, HUDView> hudViewMap;
-    protected Map<HUDComponent, HUDImageComponent> hudIconMap;
     protected HUD hud;
 
     public HUDAbsoluteLayoutManager() {
@@ -49,7 +49,6 @@ public class HUDAbsoluteLayoutManager implements HUDLayoutManager {
     public HUDAbsoluteLayoutManager(HUD hud) {
         this.hud = hud;
         hudViewMap = Collections.synchronizedMap(new HashMap());
-        hudIconMap = Collections.synchronizedMap(new HashMap());
     }
 
     /**
@@ -121,5 +120,27 @@ public class HUDAbsoluteLayoutManager implements HUDLayoutManager {
      */
     public void relayout(HUDComponent component) {
         // components are positioned absolutely
+    }
+
+    public class HUDComponentComparator implements Comparator {
+
+        public int compare(Object o1, Object o2) {
+            int r = 0;
+            if ((o1 instanceof HUDComponent) && (o2 instanceof HUDComponent)) {
+                HUDComponent c1 = (HUDComponent) o1;
+                HUDComponent c2 = (HUDComponent) o2;
+
+                if (c1.getX() < c2.getX()) {
+                    r = -1;
+                } else if (c1.getX() == c2.getX()) {
+                    r = 0;
+                } else if (c1.getX() > c2.getX()) {
+                    r = 1;
+                }
+            } else {
+                throw new ClassCastException();
+            }
+            return r;
+        }
     }
 }
