77e30664ca1c4f43fbed7f8eee8650174fc1c398
deronj <deronj@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-08-06 18:30:33 +0000

    Include frame header and footer in the placement height

--- modules/foundation/appbase/src/classes/org/jdesktop/wonderland/modules/appbase/client/cell/FirstVisibleInitializerCell.java
+++ modules/foundation/appbase/src/classes/org/jdesktop/wonderland/modules/appbase/client/cell/FirstVisibleInitializerCell.java
@@ -25,6 +25,7 @@ import org.jdesktop.wonderland.common.ExperimentalAPI;
 import org.jdesktop.wonderland.common.cell.CellTransform;
 import org.jdesktop.wonderland.modules.appbase.client.FirstVisibleInitializer;
 import java.util.logging.Logger;
+import org.jdesktop.wonderland.modules.appbase.client.cell.view.viewdefault.Frame2DCell;
 
 /**
  * Used to move an cell to its initial location before the first time something
@@ -56,8 +57,11 @@ public class FirstVisibleInitializerCell implements FirstVisibleInitializer {
     public void initialize (float width3D, float height3D) {
         logger.info("FVI.initialize, wh3D = " + width3D + ", " + height3D);
 
+        // Include the frame header and footer
+        float height = height3D + Frame2DCell.HEADER_HEIGHT + Frame2DCell.SIDE_THICKNESS;
+
         // Determine the "first visible bounds" based on the size of the first-visible window
-        BoundingBox bbox = new BoundingBox(new Vector3f(), width3D/2.0f, height3D/2.0f, 1f);
+        BoundingBox bbox = new BoundingBox(new Vector3f(), width3D/2.0f, height/2.0f, 1f);
         logger.info("new bbox = " + bbox);
         
         // Calculate the "best" initial cell transform, based on the size of the first
