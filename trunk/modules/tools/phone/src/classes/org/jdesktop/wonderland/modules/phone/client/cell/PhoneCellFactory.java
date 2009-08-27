4d0c80d6c689e33448963334861b47a79c108b75
ronnystandtke <ronnystandtke@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-08-27 15:18:07 +0000

    phone: added i18n, added L10n for locale de, code cleanup

--- trunk/modules/tools/phone/src/classes/org/jdesktop/wonderland/modules/phone/client/cell/PhoneCellFactory.java
+++ trunk/modules/tools/phone/src/classes/org/jdesktop/wonderland/modules/phone/client/cell/PhoneCellFactory.java
@@ -27,25 +27,32 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.net.URL;
 import java.util.Properties;
+import java.util.ResourceBundle;
 import org.jdesktop.wonderland.client.cell.registry.annotation.CellFactory;
 
 /**
  * The cell factory for the sample cell.
  * 
  * @author Jordan Slott <jslott@dev.java.net>
+ * @author Ronny Standtke <ronny.standtke@fhnw.ch>
  */
 @CellFactory
 public class PhoneCellFactory implements CellFactorySPI {
 
+    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(
+            "org/jdesktop/wonderland/modules/phone/client/cell/resources/" +
+            "Bundle");
+
     public String[] getExtensions() {
-        return new String[] {};
+        return new String[]{};
     }
 
-    public <T extends CellServerState> T getDefaultCellServerState(Properties props) {
+    public <T extends CellServerState> T getDefaultCellServerState(
+            Properties props) {
         // Create a setup with some default values
         PhoneCellServerState cellServerState = new PhoneCellServerState();
         cellServerState.setPhoneInfo(new PhoneInfo(false, "100", "foo",
-	    "Unknown location", .2, .1, true, true));
+                "Unknown location", .2, .1, true, true));
 
         Vector3f axis = new Vector3f((float) 1, (float) 0, (float) 0);
         /*
@@ -54,16 +61,17 @@ public class PhoneCellFactory implements CellFactorySPI {
         //cellServerState.setRotation(new Rotation(axis, (float) Math.PI / 4));
 
         Logger.getLogger(PhoneCellFactory.class.getName()).warning(
-	    "New Virtual Phone!!!!");
+                "New Virtual Phone!!!!");
         return (T) cellServerState;
     }
 
     public String getDisplayName() {
-        return "Virtual Phone";
+        return BUNDLE.getString("Virtual_Phone");
     }
 
     public Image getPreviewImage() {
-        URL url = PhoneCellFactory.class.getResource("resources/virtualphone_preview.png");
+        URL url = PhoneCellFactory.class.getResource(
+                "resources/virtualphone_preview.png");
         return Toolkit.getDefaultToolkit().createImage(url);
     }
 }
