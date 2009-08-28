1a59b1d118e0e3d0a14ed4ffc7301dbc40ad697f
paulby <paulby@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-08-28 00:37:15 +0000

    Remove debug println, issue 499

--- core/src/classes/org/jdesktop/wonderland/client/jme/cellrenderer/BasicRenderer.java
+++ core/src/classes/org/jdesktop/wonderland/client/jme/cellrenderer/BasicRenderer.java
@@ -200,7 +200,6 @@ public abstract class BasicRenderer implements CellRendererJME {
         sceneRoot = createSceneGraph(ret);
         rootNode.attachChild(sceneRoot);
         applyTransform(rootNode, cell.getLocalTransform());
-        System.err.println("Setting root transform "+cell.getLocalTransform());
         addRenderState(rootNode);
 
         addDefaultComponents(ret, rootNode);
@@ -240,7 +239,6 @@ public abstract class BasicRenderer implements CellRendererJME {
 
         if (rootNode!=null) {
             rootNode.updateWorldBound();
-            System.err.println("BOUNDS "+rootNode.getWorldBound());
 
             // Some subclasses (like the imi collada renderer) already add
             // a render component
