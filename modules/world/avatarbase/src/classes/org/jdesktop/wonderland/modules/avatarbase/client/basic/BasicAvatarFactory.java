f03767ac5c122de28eb7c3a6d1da2dd7f9b9ef04
ronnystandtke <ronnystandtke@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-09-10 15:03:54 +0000

    avatarbase: small i18n/L10n fixes, very careful and reviewed code cleanup

--- modules/world/avatarbase/src/classes/org/jdesktop/wonderland/modules/avatarbase/client/basic/BasicAvatarFactory.java
+++ modules/world/avatarbase/src/classes/org/jdesktop/wonderland/modules/avatarbase/client/basic/BasicAvatarFactory.java
@@ -18,6 +18,7 @@
 package org.jdesktop.wonderland.modules.avatarbase.client.basic;
 
 import java.util.HashSet;
+import java.util.ResourceBundle;
 import java.util.Set;
 import org.jdesktop.wonderland.client.login.ServerSessionManager;
 import org.jdesktop.wonderland.modules.avatarbase.client.registry.AvatarRegistry;
@@ -32,36 +33,40 @@ import org.jdesktop.wonderland.modules.avatarbase.client.registry.spi.AvatarFact
 @AvatarFactory
 public class BasicAvatarFactory implements AvatarFactorySPI {
 
+    private static final ResourceBundle BUNDLE =
+            ResourceBundle.getBundle("org/jdesktop/wonderland/modules/" +
+            "avatarbase/client/resources/Bundle");
+
     // The set of basic avatars
     private static Set<BasicAvatar> basicAvatarSet = null;
-
+    
     // A hard-coded list of relative URLs of the basic avatar artwork
     private static String AVATARS[][] = {
         {
-            "Cartoon (Male)",
+            BUNDLE.getString("Cartoon_Male"),
             "default-avatars/maleCartoonAvatar.dae/maleCartoonAvatar.dae.gz.dep"
         },
         {
-            "Cartoon (Female)",
+            BUNDLE.getString("Cartoon_Female"),
             "default-avatars/femaleCartoonAvatar.dae/femaleCartoonAvatar.dae.gz.dep"
         },
         {
-            "Toy (Male)",
+            BUNDLE.getString("Toy_Male"),
             "default-avatars/maleToyAvatar.dae/maleToyAvatar.dae.gz.dep"
         },
         {
-            "Toy (Female)",
+            BUNDLE.getString("Toy_Female"),
             "default-avatars/femaleToyAvatar.dae/femaleToyAvatar.dae.gz.dep"
         }
     };
-    
+
     /**
      * {@inheritDoc}
      */
     public void registerAvatars(ServerSessionManager session) {
         // Create the set of basic avatars from the hard-coded list of URLs
         AvatarRegistry registry = AvatarRegistry.getAvatarRegistry();
-        basicAvatarSet = new HashSet();
+        basicAvatarSet = new HashSet<BasicAvatar>();
         for (int i = 0; i < AVATARS.length; i++) {
             BasicAvatar avatar = new BasicAvatar(AVATARS[i][0], AVATARS[i][1]);
             basicAvatarSet.add(avatar);
