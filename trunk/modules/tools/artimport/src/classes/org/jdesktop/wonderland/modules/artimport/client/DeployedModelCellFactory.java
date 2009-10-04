652b0a516666587456beddae811a4f950e215af2
jslott <jslott@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-10-04 14:25:02 +0000

    Partial Fix Issue #635: Added Cell Factory to handle .dep files that use the LoaderManager to load a Cell, but still get the following exception:
         [java] Oct 4, 2009 1:03:09 AM org.jdesktop.wonderland.modules.jmecolladaloader.client.JmeColladaLoader loadDeployedModel
         [java] WARNING: No Loader data for model wlcontent://users@jordan-slotts-macbook-pro.local:8080/Jordan/art/satellite.kmz/satellite.kmz.dep
         [java] com.jmex.xml.xml.XmlException: Value of VersionType is invalid.
         [java] Oct 4, 2009 1:03:09 AM com.jmex.model.collada.ThreadSafeColladaImporter load
         [java] SEVERE: Unable to load Collada file. 
         [java] com.jmex.xml.xml.XmlException: Value of VersionType is invalid.
         [java] 	at com.jmex.model.collada.schema.VersionType.validate(VersionType.java:60)
         [java] 	at com.jmex.model.collada.schema.VersionType.<init>(VersionType.java:33)
         [java] 	at com.jmex.model.collada.schema.COLLADAType.getversionAt(COLLADAType.java:202)
         [java] 	at com.jmex.model.collada.schema.COLLADAType.getversion(COLLADAType.java:222)
         [java] 	at com.jmex.model.collada.ThreadSafeColladaImporter.load(ThreadSafeColladaImporter.java:272)
         [java] 	at org.jdesktop.wonderland.modules.jmecolladaloader.client.JmeColladaLoader.loadModel(JmeColladaLoader.java:126)
         [java] 	at org.jdesktop.wonderland.modules.jmecolladaloader.client.JmeColladaLoader.loadDeployedModel(JmeColladaLoader.java:211)
         [java] 	at org.jdesktop.wonderland.client.cell.ModelCellComponent.loadModel(ModelCellComponent.java:103)
         [java] 	at org.jdesktop.wonderland.client.jme.cellrenderer.ModelRenderer.createSceneGraph(ModelRenderer.java:76)
         [java] 	at org.jdesktop.wonderland.client.jme.cellrenderer.BasicRenderer.createEntity(BasicRenderer.java:206)
         [java] 	at org.jdesktop.wonderland.client.jme.cellrenderer.BasicRenderer.getEntity(BasicRenderer.java:362)
         [java] 	at org.jdesktop.wonderland.client.jme.cellrenderer.BasicRenderer.setStatus(BasicRenderer.java:116)
         [java] 	at org.jdesktop.wonderland.client.cell.Cell.setRendererStatus(Cell.java:570)
         [java] 	at org.jdesktop.wonderland.client.cell.Cell.createCellRendererImpl(Cell.java:879)
         [java] 	at org.jdesktop.wonderland.client.cell.Cell.setStatus(Cell.java:660)
         [java] 	at org.jdesktop.wonderland.client.cell.CellCacheBasicImpl.setCellStatus(CellCacheBasicImpl.java:344)
         [java] 	at org.jdesktop.wonderland.client.cell.CellCacheBasicImpl.access$000(CellCacheBasicImpl.java:50)
         [java] 	at org.jdesktop.wonderland.client.cell.CellCacheBasicImpl$CellStatusChanger.run(CellCacheBasicImpl.java:481)
         [java] 	at java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:441)
         [java] 	at java.util.concurrent.FutureTask$Sync.innerRun(FutureTask.java:303)
         [java] 	at java.util.concurrent.FutureTask.run(FutureTask.java:138)
         [java] 	at java.util.concurrent.ThreadPoolExecutor$Worker.runTask(ThreadPoolExecutor.java:886)
         [java] 	at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:908)
         [java] 	at java.lang.Thread.run(Thread.java:637)

---
+++ trunk/modules/tools/artimport/src/classes/org/jdesktop/wonderland/modules/artimport/client/DeployedModelCellFactory.java
@@ -0,0 +1,129 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath"
+ * exception as provided by Sun in the License file that accompanied
+ * this code.
+ */
+package org.jdesktop.wonderland.modules.artimport.client;
+
+import com.jme.math.Quaternion;
+import com.jme.math.Vector3f;
+import java.awt.Image;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jdesktop.wonderland.client.cell.asset.AssetUtils;
+import org.jdesktop.wonderland.client.cell.registry.annotation.CellFactory;
+import org.jdesktop.wonderland.client.cell.registry.spi.CellFactorySPI;
+import org.jdesktop.wonderland.client.jme.artimport.DeployedModel;
+import org.jdesktop.wonderland.client.jme.artimport.LoaderManager;
+import org.jdesktop.wonderland.client.jme.artimport.ModelLoader;
+import org.jdesktop.wonderland.common.cell.state.CellServerState;
+import org.jdesktop.wonderland.common.cell.state.ModelCellServerState;
+
+/**
+ * A Cell Factory that loads deployed model (.dep) files. This does not appear
+ * in the Cell Palette, but supports creation via DnD or the Content Browser.
+ *
+ * @author Jordan Slott <jslott@dev.java.net>
+ */
+@CellFactory
+public class DeployedModelCellFactory implements CellFactorySPI {
+
+    // The error logger
+    private static final Logger LOGGER =
+            Logger.getLogger(DeployedModelCellFactory.class.getName());
+
+    /**
+     * {@inheritDoc}
+     */
+    public String[] getExtensions() {
+        return new String[] { "dep" };
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public <T extends CellServerState> T getDefaultCellServerState(Properties props) {
+        // Fetch the URI of the model to load via the "content-uri" property.
+        // If not present return null. Convert into a URL
+        URL url = null;
+        if (props != null) {
+            String uri = props.getProperty("content-uri");
+            if (uri != null) {
+                try {
+                    url = AssetUtils.getAssetURL(uri);
+                } catch (MalformedURLException excp) {
+                    LOGGER.log(Level.WARNING, "Unable to form asset URI from " +
+                            uri, excp);
+                    url = null;
+                }
+            }
+        }
+
+        // Check to make sure the URL is not null. If so, then just return
+        // null
+        if (url == null) {
+            LOGGER.warning("The URL is null");
+            return null;
+        }
+
+
+        LOGGER.warning("Loading URL " + url.toExternalForm());
+
+        // Simply create a new ModelCell by creating a ModelCellServerState
+        // with the URL passed in via the properties. First load the deployed
+        // model from the given URL.
+        LoaderManager lm = LoaderManager.getLoaderManager();
+        DeployedModel dm = null;
+        try {
+            dm = lm.getLoaderFromDeployment(url);
+        } catch (IOException excp) {
+            LOGGER.log(Level.WARNING, "Unable to load deployed model from " +
+                    url.toExternalForm(), excp);
+            return null;
+        }
+        
+        // Get the loader from the deployed model. Use that to create a
+        // server state
+        ModelLoader loader = dm.getModelLoader();
+        return (T) loader.getCellServerState(
+                url.toExternalForm(),            // Model URL
+                Vector3f.ZERO,                   // Translation
+                new Quaternion(),                // Rotation
+                new Vector3f(1.0f, 1.0f, 1.0f),  // Scale
+                null                             // Properties
+                );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getDisplayName() {
+        // Should not appear in the Cell Palette so return null
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Image getPreviewImage() {
+        // Does not appear in the Cell Palette, so no preview image
+        return null;
+    }
+
+}
