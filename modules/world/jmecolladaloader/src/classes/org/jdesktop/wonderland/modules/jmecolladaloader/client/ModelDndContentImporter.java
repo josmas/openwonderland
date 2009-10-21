999282f870402f4c040121efb847463d42efdff9
jslott <jslott@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-10-21 00:10:44 +0000

    Fix Issue #673: DnD of models now fully works -- users can select Replace or Use Existing and it will work.

--- modules/world/jmecolladaloader/src/classes/org/jdesktop/wonderland/modules/jmecolladaloader/client/ModelDndContentImporter.java
+++ modules/world/jmecolladaloader/src/classes/org/jdesktop/wonderland/modules/jmecolladaloader/client/ModelDndContentImporter.java
@@ -17,11 +17,6 @@
  */
 package org.jdesktop.wonderland.modules.jmecolladaloader.client;
 
-import com.jme.bounding.BoundingBox;
-import com.jme.bounding.BoundingSphere;
-import com.jme.bounding.BoundingVolume;
-import com.jme.math.Quaternion;
-import com.jme.math.Vector3f;
 import com.jme.scene.Node;
 import org.jdesktop.wonderland.client.jme.artimport.*;
 import java.io.File;
@@ -29,25 +24,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
 import org.jdesktop.mtgame.Entity;
 import org.jdesktop.mtgame.RenderComponent;
-import org.jdesktop.wonderland.client.cell.utils.CellCreationException;
-import org.jdesktop.wonderland.client.cell.utils.CellUtils;
-import org.jdesktop.wonderland.client.content.spi.ContentImporterSPI;
 import org.jdesktop.wonderland.client.jme.ClientContextJME;
-import org.jdesktop.wonderland.client.jme.JmeClientMain;
+import org.jdesktop.wonderland.client.jme.content.AbstractContentImporter;
 import org.jdesktop.wonderland.client.login.ServerSessionManager;
 import org.jdesktop.wonderland.common.FileUtils;
-import org.jdesktop.wonderland.common.cell.state.BoundingVolumeHint;
-import org.jdesktop.wonderland.common.cell.state.CellServerState;
-import org.jdesktop.wonderland.common.cell.state.ModelCellComponentServerState;
-import org.jdesktop.wonderland.common.cell.state.ModelCellServerState;
-import org.jdesktop.wonderland.common.cell.state.PositionComponentServerState;
-import org.jdesktop.wonderland.common.cell.state.PositionComponentServerState.Bounds;
 import org.jdesktop.wonderland.modules.contentrepo.client.ContentRepository;
 import org.jdesktop.wonderland.modules.contentrepo.client.ContentRepositoryRegistry;
 import org.jdesktop.wonderland.modules.contentrepo.common.ContentCollection;
@@ -55,17 +37,20 @@ import org.jdesktop.wonderland.modules.contentrepo.common.ContentNode;
 import org.jdesktop.wonderland.modules.contentrepo.common.ContentNode.Type;
 import org.jdesktop.wonderland.modules.contentrepo.common.ContentRepositoryException;
 import org.jdesktop.wonderland.modules.contentrepo.common.ContentResource;
-import org.jdesktop.wonderland.modules.jmecolladaloader.common.cell.state.JmeColladaCellComponentServerState;
 
 /**
- * A content importer handler for ModelLoaders
+ * A content importer handler for Model Loaders. This will deploy and upload
+ * the file to WebDav if necessary and create a Cell to display the model.
  *
  * @author paulby
  * @author Jordan Slott <jslott@dev.java.net>
  */
-public class ModelDndContentImporter implements ContentImporterSPI {
+public class ModelDndContentImporter extends AbstractContentImporter {
 
-    private static Logger logger = Logger.getLogger(ModelDndContentImporter.class.getName());
+    // The error logger
+    private static final Logger LOGGER =
+            Logger.getLogger(ModelDndContentImporter.class.getName());
+
     private ServerSessionManager loginInfo = null;
     private String[] extensions;
 
@@ -75,108 +60,18 @@ public class ModelDndContentImporter implements ContentImporterSPI {
         this.extensions = extensions;
     }
 
-    @Override
-    public String importFile(File file, String extension) {
-
-        final JFrame frame = JmeClientMain.getFrame().getFrame();
-
-        // Next check whether the content already exists and ask the user if
-        // the upload should still proceed. By default (result == 0), the
-        // system will upload (and overwrite) and files.
-        int result = JOptionPane.YES_OPTION;
-        ContentResource resource = isContentExists(file);
-        if (resource != null) {
-            Object[] options = {"Replace", "Use Existing", "Cancel" };
-            String msg = "The file " + file.getName() + " already exists in" +
-                    "the content repository. Do you wish to replace it and " +
-                    "continue?";
-            String title = "Replace Content?";
-            result = JOptionPane.showOptionDialog(frame, msg, title,
-                    JOptionPane.YES_NO_CANCEL_OPTION,
-                    JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
-
-            // If the user hits Cancel or a "closed" action (e.g. Escape key)
-            // then just return
-            if (result == JOptionPane.CANCEL_OPTION || result == JOptionPane.CLOSED_OPTION) {
-                return null;
-            }
-        }
-
-        // If the content exists and we do not want to upload a new version,
-        // then simply create it and return.
-        if (result == JOptionPane.NO_OPTION) {
-            URL url = null;
-            try {
-//                JOptionPane.showMessageDialog(frame, "Use Existing is not currently supported");
-//                return null;
-
-                // THIS IS WRONG, we need to import the model so that the
-                // server state is set correctly in the deployedModel
-                url = resource.getURL();
-                LoaderManager manager = LoaderManager.getLoaderManager();
-                DeployedModel dm = manager.getLoaderFromDeployment(url);
-                createCell(dm);
-                return dm.getModelURL();
-            } catch (java.lang.Exception excp) {
-                logger.log(Level.WARNING, "Unable to load existing model, url=" +
-                        url, excp);
-                JOptionPane.showMessageDialog(frame,
-                        "Failed to display existing model " + file.getAbsolutePath(),
-                        "Display Failed",
-                        JOptionPane.ERROR_MESSAGE);
-                return null;
-            }
-        }
-
-        // Display a dialog showing a wait message while we import. We need
-        // to do this in the SwingWorker thread so it gets drawn
-        JOptionPane waitMsg = new JOptionPane("Please wait while " +
-                file.getName() + " is being uploaded");
-        final JDialog dialog = waitMsg.createDialog(frame, "Uploading Content");
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                dialog.setVisible(true);
-            }
-        });
-
-        // Next, do the actual upload of the file. This should display a
-        // progress dialog if the upload is going to take a long time.
-        DeployedModel deployedModel;
-        try {
-            deployedModel = modelUploadContent(file);
-        } catch (java.io.IOException excp) {
-            logger.log(Level.WARNING, "Failed to upload content file " +
-                    file.getAbsolutePath(), excp);
-
-            final String fileName = file.getName();
-            SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
-                    dialog.setVisible(false);
-                    JOptionPane.showMessageDialog(frame,
-                            "Failed to upload content file " + fileName,
-                            "Upload Failed", JOptionPane.ERROR_MESSAGE);
-                }
-            });
-            return null;
-        } finally {
-            // Close down the dialog indicating success
-            SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
-                    dialog.setVisible(false);
-                }
-            });
-        }
-
-        // Finally, go ahead and create the cell.
-        createCell(deployedModel);
-        return deployedModel.getModelURL();
+    /**
+     * @inheritDoc()
+     */
+    public String[] getExtensions() {
+        return extensions;
     }
 
     /**
-     * Check to see if the model already exists on the server. If so, return
-     * the ContentResource of the model's .dep file, or null otherwise.
+     * {@inheritDoc}
      */
-    private ContentResource isContentExists(File file) {
+    @Override
+    public String isContentExists(File file) {
         // Check to see if the model already exists on the server. It should
         // be under a file named: <filename>/<filename>.dep. If so, then return
         // the ContentResource that points to the file, otherwise, return null.
@@ -185,20 +80,21 @@ public class ModelDndContentImporter implements ContentImporterSPI {
         try {
             ContentNode node = userRoot.getChild(fileName);
             if (node != null && node instanceof ContentResource) {
-                return (ContentResource)node;
+                return getModelURI(file);
             }
             return null;
         } catch (ContentRepositoryException excp) {
-            logger.log(Level.WARNING, "Error while try to find " + fileName +
+            LOGGER.log(Level.WARNING, "Error while try to find " + fileName +
                     " in content repository", excp);
             return null;
         }
     }
 
     /**
-     * @inheritDoc()
+     * {@inheritDoc}
      */
-    public DeployedModel modelUploadContent(File file) throws IOException {
+    @Override
+    public String uploadContent(File file) throws IOException {
         URL url = file.toURI().toURL();
         ModelLoader loader = LoaderManager.getLoaderManager().getLoader(url);
         ImportSettings importSettings = new ImportSettings(url);
@@ -230,7 +126,9 @@ public class ModelDndContentImporter implements ContentImporterSPI {
         filename = filename.substring(0, filename.lastIndexOf('.'));
         importedModel.setWonderlandName(filename);
 
-        DeployedModel deployedModel = loader.deployToModule(tmpDir, importedModel);
+        // Deploy the model to a module as an intermediate step. Write to a
+        // directory.
+        loader.deployToModule(tmpDir, importedModel);
 
         // Now copy the temporary files into webdav
 
@@ -243,14 +141,12 @@ public class ModelDndContentImporter implements ContentImporterSPI {
             // Copy from the art directory
             File artDir = FileUtils.findDir(tmpDir, "art");
             copyFiles(artDir, modelRoot);
-        } catch (ContentRepositoryException ex) {
-            Logger.getLogger(ModelDndContentImporter.class.getName()).log(Level.SEVERE, null, ex);
+            return getModelURI(file);
+        } catch (ContentRepositoryException excp) {
+            String msg = "Failed to upload model file " + file.getName();
+            LOGGER.log(Level.WARNING, msg, excp);
+            throw new IOException(msg);
         }
-
-        ModelCellServerState cellState = (ModelCellServerState) deployedModel.getCellServerState();
-        cellState.setName(importedModel.getWonderlandName());
-
-        return deployedModel;
     }
 
     /**
@@ -302,61 +198,26 @@ public class ModelDndContentImporter implements ContentImporterSPI {
     }
 
     /**
-     * @inheritDoc()
-     */
-    public String[] getExtensions() {
-        return extensions;
-    }
-
-    /**
      * Returns the content repository root for the current user, or null upon
      * error.
      */
     private ContentCollection getUserRoot() {
-        ContentRepositoryRegistry registry = ContentRepositoryRegistry.getInstance();
-        ContentRepository repo = registry.getRepository(loginInfo);
+        ContentRepositoryRegistry reg = ContentRepositoryRegistry.getInstance();
+        ContentRepository repo = reg.getRepository(loginInfo);
         try {
             return repo.getUserRoot();
         } catch (ContentRepositoryException excp) {
-            logger.log(Level.WARNING, "Unable to find repository root", excp);
+            LOGGER.log(Level.WARNING, "Unable to find repository root", excp);
             return null;
         }
     }
 
-    public void createCell(DeployedModel deployedModel) {
-        // Fetch the bounds of the Cell and use it as the bounds hint.
-        CellServerState state = deployedModel.getCellServerState();
-        if (state==null) {
-            state = deployedModel.getModelLoader().getCellServerState("Foo", Vector3f.ZERO, new Quaternion(), new Vector3f(1,1,1), null);
-        }
-        PositionComponentServerState pcss =
-                (PositionComponentServerState) state.getComponentServerState(PositionComponentServerState.class);
-        BoundingVolume boundsHint = null;
-        if (pcss != null) {
-            Bounds bounds = pcss.getBounds();
-            if (bounds.type == Bounds.BoundsType.BOX) {
-                if (bounds.x>20 || bounds.y>20 || bounds.z>20) {
-                    boundsHint = new BoundingBox(Vector3f.ZERO, 1, 1, 1);
-
-                } else {
-                    boundsHint = new BoundingBox(Vector3f.ZERO, (float)bounds.x,
-                            (float)bounds.y, (float)bounds.z);
-                }
-            }
-            else {
-                if (bounds.x>20)
-                    boundsHint = new BoundingSphere(1f, Vector3f.ZERO);
-                else
-                    boundsHint = new BoundingSphere((float)bounds.x, Vector3f.ZERO);
-            }
-        }
-        BoundingVolumeHint hint = new BoundingVolumeHint(true, boundsHint);
-        state.setBoundingVolumeHint(hint);
-
-        try {
-            CellUtils.createCell(state);
-        } catch (CellCreationException excp) {
-            logger.log(Level.WARNING, "Unable to create cell for uri " + deployedModel.getModelURL(), excp);
-        }
+    /**
+     * Returns the String URI of the deployed model (.dep) for the given local
+     * file.
+     */
+    private String getModelURI(File file) {
+        return "wlcontent://users/" + loginInfo.getUsername() + "/art/" +
+                file.getName() + "/" + file.getName() + ".dep";
     }
 }
