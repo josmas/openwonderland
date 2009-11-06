e96892cc98f2184c1611461e0424e9d69d352ae6
kaplanj <jonathankap@users.noreply.github.com>
2009-11-06 01:34:01 +0000

    Branch for presence manager updates to improve scalability

---
+++ presence-manager/modules/tools/microphone/src/classes/org/jdesktop/wonderland/modules/microphone/common/MicrophoneCellClientState.java
@@ -0,0 +1,36 @@
+/**
+ * Project Wonderland
+ *
+ * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * Sun designates this particular file as subject to the "Classpath" 
+ * exception as provided by Sun in the License file that accompanied 
+ * this code.
+ */
+package org.jdesktop.wonderland.modules.microphone.common;
+
+import java.io.Serializable;
+
+import org.jdesktop.wonderland.common.cell.state.CellClientState;
+
+/**
+ * The MicrophoneCellSetup class is the cell that renders a microphone cell in
+ * world.
+ * 
+ * @author jkaplan
+ */
+public class MicrophoneCellClientState extends CellClientState {
+
+    /** Default constructor */
+    public MicrophoneCellClientState() {
+    }
+    
+}
