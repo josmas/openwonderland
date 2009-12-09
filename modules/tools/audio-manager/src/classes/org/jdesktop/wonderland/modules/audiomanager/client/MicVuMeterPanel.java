7e425d2e561ad938d2dc70df84c1b80f9e0abfdc
jprovino <jprovino@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-12-09 23:50:34 +0000

    Changes for muting mic and remembering volume level.

--- modules/tools/audio-manager/src/classes/org/jdesktop/wonderland/modules/audiomanager/client/MicVuMeterPanel.java
+++ modules/tools/audio-manager/src/classes/org/jdesktop/wonderland/modules/audiomanager/client/MicVuMeterPanel.java
@@ -17,15 +17,13 @@
  */
 package org.jdesktop.wonderland.modules.audiomanager.client;
 
-import java.awt.Color;
 import org.jdesktop.wonderland.modules.audiomanager.common.VolumeConverter;
 
 import java.io.IOException;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.ImageIcon;
-import org.jdesktop.wonderland.client.jme.VMeter;
+import org.jdesktop.wonderland.client.jme.Meter;
 import org.jdesktop.wonderland.client.softphone.MicrophoneInfoListener;
 import org.jdesktop.wonderland.client.softphone.SoftphoneControl;
 import org.jdesktop.wonderland.client.softphone.SoftphoneControlImpl;
@@ -37,28 +35,18 @@ import org.jdesktop.wonderland.client.softphone.SoftphoneListener;
  * @author jp
  * @author nsimpson
  */
-public class MicVuMeterPanel extends javax.swing.JPanel implements
+public class MicVuMeterPanel extends javax.swing.JPanel implements 
         SoftphoneListener, MicrophoneInfoListener, DisconnectListener {
 
     private static final Logger LOGGER =
             Logger.getLogger(MicVuMeterPanel.class.getName());
-    private static final double DEFAULT_WARNING_LIMIT = 0.9d;
-    private static final int VU_COUNT = 10;
     private AudioManagerClient client;
-    private VMeter micMeter;
-    private VMeter speakerMeter;
+    private Meter meter;
+    private static final int VU_COUNT = 10;
     private int count;
     private double volume;
+
     private VolumeConverter volumeConverter;
-    private Color micPanelBackground;
-    private Color speakerPanelBackground;
-    private Color overLimitColor = Color.RED;
-    private double micWarningLimit = DEFAULT_WARNING_LIMIT;
-    private double speakerWarningLimit = DEFAULT_WARNING_LIMIT;
-    private ImageIcon micMutedIcon;
-    private ImageIcon micUnmutedIcon;
-    private ImageIcon speakerMutedIcon;
-    private ImageIcon speakerUnmutedIcon;
 
     public MicVuMeterPanel() {
         this(null);
@@ -68,61 +56,31 @@ public class MicVuMeterPanel extends javax.swing.JPanel implements
         this.client = client;
 
         initComponents();
-        micPanelBackground = micMeterPanel.getBackground();
-        speakerPanelBackground = speakerMeterPanel.getBackground();
-        micMutedIcon = new ImageIcon(getClass().getResource(
-                "/org/jdesktop/wonderland/modules/audiomanager/client/" +
-                "resources/UserListMicMuteOn24x24.png"));
-        micUnmutedIcon = new ImageIcon(getClass().getResource(
-                "/org/jdesktop/wonderland/modules/audiomanager/client/" +
-                "resources/UserListMicMuteOff24x24.png"));
-        speakerMutedIcon = new ImageIcon(getClass().getResource(
-                "/org/jdesktop/wonderland/modules/audiomanager/client/" +
-                "resources/UserListSpeakerMuteOn24x24.png"));
-        speakerUnmutedIcon = new ImageIcon(getClass().getResource(
-                "/org/jdesktop/wonderland/modules/audiomanager/client/" +
-                "resources/UserListSpeakerMuteOff24x24.png"));
 
-        volumeConverter = new VolumeConverter(micVolumeSlider.getMaximum());
+	volumeConverter = new VolumeConverter(volumeSlider.getMaximum());
 
         if (client != null) {
             client.addDisconnectListener(this);
         }
 
-        // microphone volume meter
-        micMeter = new VMeter("");
-        micMeter.setBackground(Color.WHITE);
-        micMeter.setForeground(Color.DARK_GRAY);
-        micMeter.setPreferredSize(micMeterPanel.getPreferredSize());
-        micMeter.setShowValue(false);
-        micMeter.setShowTicks(false);
-        micMeter.setMaxValue(1D);
-        micMeter.setWarningValue(micWarningLimit);
-        micMeter.setVisible(true);
-        micMeterPanel.add(micMeter);
+        meter = new Meter("");
+        meter.setPreferredSize(vuMeterPanel.getPreferredSize());
+        meter.setMaxValue(1D);
+        meter.setWarningValue(0.90);
+        meter.setVisible(true);
 
-        // speaker volume meter
-        speakerMeter = new VMeter("");
-        speakerMeter.setBackground(Color.WHITE);
-        speakerMeter.setForeground(Color.DARK_GRAY);
-        speakerMeter.setPreferredSize(micMeterPanel.getPreferredSize());
-        speakerMeter.setShowValue(false);
-        speakerMeter.setShowTicks(false);
-        speakerMeter.setMaxValue(1D);
-        speakerMeter.setWarningValue(speakerWarningLimit);
-        speakerMeter.setVisible(true);
-        speakerMeterPanel.add(speakerMeter);
+        vuMeterPanel.add(meter);
+        validate();
+        vuMeterPanel.setVisible(true);
 
-        startMicVuMeter(true);
-        startSpeakerVuMeter(true);
+        startVuMeter(true);
     }
 
     public void disconnected() {
-        startMicVuMeter(false);
-        startSpeakerVuMeter(false);
+        startVuMeter(false);
     }
 
-    public void startMicVuMeter(final boolean startVuMeter) {
+    public void startVuMeter(final boolean startVuMeter) {
         SoftphoneControl sc = SoftphoneControlImpl.getInstance();
 
         client.removeDisconnectListener(this);
@@ -153,22 +111,10 @@ public class MicVuMeterPanel extends javax.swing.JPanel implements
         });
     }
 
-    public void startSpeakerVuMeter(final boolean startVuMeter) {
-        // TODO: set up monitor for speaker volume
-        if (startVuMeter) {
-
-        }
-    }
-
     public void softphoneVisible(boolean isVisible) {
     }
 
     public void softphoneMuted(boolean muted) {
-        if (SoftphoneControlImpl.getInstance().isMuted()) {
-            micMuteButton.setIcon(micMutedIcon);
-        } else {
-            micMuteButton.setIcon(micUnmutedIcon);
-        }
     }
 
     public void softphoneConnected(boolean connected) {
@@ -190,12 +136,7 @@ public class MicVuMeterPanel extends javax.swing.JPanel implements
             java.awt.EventQueue.invokeLater(new Runnable() {
 
                 public void run() {
-                    micMeter.setValue(volume);
-                    if (volume > micWarningLimit) {
-                        micMeterPanel.setBackground(overLimitColor);
-                    } else {
-                        micMeterPanel.setBackground(micPanelBackground);
-                    }
+                    meter.setValue(volume);
                 }
             });
 
@@ -212,7 +153,7 @@ public class MicVuMeterPanel extends javax.swing.JPanel implements
     }
 
     public void microphoneVolume(String data) {
-        micVolumeSlider.setValue(volumeConverter.getVolume((Float.parseFloat(data))));
+        volumeSlider.setValue(volumeConverter.getVolume((Float.parseFloat(data))));
     }
 
     /** This method is called from within the constructor to
@@ -224,73 +165,62 @@ public class MicVuMeterPanel extends javax.swing.JPanel implements
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        micVolumeSlider = new javax.swing.JSlider();
-        micMeterPanel = new javax.swing.JPanel();
-        micMuteButton = new javax.swing.JButton();
-        speakerVolumeSlider = new javax.swing.JSlider();
-        speakerMeterPanel = new javax.swing.JPanel();
-        speakerMuteButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        vuMeterPanel = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        volumeSlider = new javax.swing.JSlider();
 
-        setPreferredSize(new java.awt.Dimension(95, 205));
-        setLayout(null);
+        java.util.ResourceBundle bundle = java.util.ResourceBundle.getBundle("org/jdesktop/wonderland/modules/audiomanager/client/resources/Bundle"); // NOI18N
+        jLabel1.setText(bundle.getString("MicVuMeterPanel.jLabel1.text")); // NOI18N
 
-        micVolumeSlider.setMinorTickSpacing(10);
-        micVolumeSlider.setOrientation(javax.swing.JSlider.VERTICAL);
-        micVolumeSlider.setPaintTicks(true);
-        micVolumeSlider.addChangeListener(new javax.swing.event.ChangeListener() {
+        vuMeterPanel.setMinimumSize(new java.awt.Dimension(230, 26));
+        vuMeterPanel.setPreferredSize(new java.awt.Dimension(230, 26));
+
+        jLabel2.setFont(jLabel2.getFont());
+        jLabel2.setText(bundle.getString("MicVuMeterPanel.jLabel2.text")); // NOI18N
+
+        volumeSlider.setMinorTickSpacing(10);
+        volumeSlider.setPaintTicks(true);
+        volumeSlider.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
-                micVolumeSliderStateChanged(evt);
+                volumeSliderStateChanged(evt);
             }
         });
-        add(micVolumeSlider);
-        micVolumeSlider.setBounds(5, 14, 20, 155);
 
-        micMeterPanel.setMinimumSize(new java.awt.Dimension(30, 160));
-        micMeterPanel.setPreferredSize(new java.awt.Dimension(30, 160));
-        add(micMeterPanel);
-        micMeterPanel.setBounds(20, 10, 20, 160);
-
-        micMuteButton.setFont(new java.awt.Font("Arial", 1, 8)); // NOI18N
-        micMuteButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/jdesktop/wonderland/modules/audiomanager/client/resources/UserListMicMuteOff24x24.png"))); // NOI18N
-        micMuteButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                micMuteButtonActionPerformed(evt);
-            }
-        });
-        add(micMuteButton);
-        micMuteButton.setBounds(20, 175, 24, 24);
-
-        speakerVolumeSlider.setMinorTickSpacing(10);
-        speakerVolumeSlider.setOrientation(javax.swing.JSlider.VERTICAL);
-        speakerVolumeSlider.setPaintTicks(true);
-        speakerVolumeSlider.addChangeListener(new javax.swing.event.ChangeListener() {
-            public void stateChanged(javax.swing.event.ChangeEvent evt) {
-                speakerVolumeSliderStateChanged(evt);
-            }
-        });
-        add(speakerVolumeSlider);
-        speakerVolumeSlider.setBounds(50, 14, 20, 155);
-
-        speakerMeterPanel.setMinimumSize(new java.awt.Dimension(30, 160));
-        speakerMeterPanel.setPreferredSize(new java.awt.Dimension(30, 160));
-        add(speakerMeterPanel);
-        speakerMeterPanel.setBounds(65, 10, 20, 160);
-
-        speakerMuteButton.setFont(new java.awt.Font("Arial", 1, 8)); // NOI18N
-        speakerMuteButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/jdesktop/wonderland/modules/audiomanager/client/resources/UserListSpeakerMuteOff24x24.png"))); // NOI18N
-        speakerMuteButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                speakerMuteButtonActionPerformed(evt);
-            }
-        });
-        add(speakerMuteButton);
-        speakerMuteButton.setBounds(65, 175, 24, 24);
+        org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(layout.createSequentialGroup()
+                .addContainerGap()
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(jLabel2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 77, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                    .add(jLabel1))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
+                    .add(vuMeterPanel, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 224, Short.MAX_VALUE)
+                    .add(volumeSlider, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 224, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(layout.createSequentialGroup()
+                .addContainerGap()
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(jLabel2)
+                    .add(vuMeterPanel, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.CENTER)
+                    .add(jLabel1)
+                    .add(volumeSlider, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 27, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void micVolumeSliderStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_micVolumeSliderStateChanged
+    private void volumeSliderStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_volumeSliderStateChanged
         SoftphoneControl sc = SoftphoneControlImpl.getInstance();
 
-        float volume = volumeConverter.getVolume(micVolumeSlider.getValue());
+        float volume = volumeConverter.getVolume(volumeSlider.getValue());
 
         try {
             sc.sendCommandToSoftphone("microphoneVolume=" + volume);
@@ -298,37 +228,12 @@ public class MicVuMeterPanel extends javax.swing.JPanel implements
             LOGGER.log(Level.WARNING,
                     "Unable to send microphone volume command to softphone", e);
         }
-    }//GEN-LAST:event_micVolumeSliderStateChanged
+    }//GEN-LAST:event_volumeSliderStateChanged
 
-    private void speakerVolumeSliderStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_speakerVolumeSliderStateChanged
-        SoftphoneControl sc = SoftphoneControlImpl.getInstance();
-
-        float volume = volumeConverter.getVolume(speakerVolumeSlider.getValue());
-        // TODO: change speaker volume
-    }//GEN-LAST:event_speakerVolumeSliderStateChanged
-
-    private void micMuteButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_micMuteButtonActionPerformed
-        SoftphoneControlImpl sc = SoftphoneControlImpl.getInstance();
-        sc.mute(!sc.isMuted());
-        // TODO: EXTRA CREDIT:
-        // - set mic volume slider to zero when muted
-        // - unmute when slider dragged
-        // - restore slider position when unmuted
-    }//GEN-LAST:event_micMuteButtonActionPerformed
-
-    private void speakerMuteButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_speakerMuteButtonActionPerformed
-        // TODO: handle muting of speaker
-        // TODO: EXTRA CREDIT:
-        // - set speaker volume slider to zero when muted
-        // - unmute when slider dragged
-        // - restore slider position when unmuted
-    }//GEN-LAST:event_speakerMuteButtonActionPerformed
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel micMeterPanel;
-    private javax.swing.JButton micMuteButton;
-    private javax.swing.JSlider micVolumeSlider;
-    private javax.swing.JPanel speakerMeterPanel;
-    private javax.swing.JButton speakerMuteButton;
-    private javax.swing.JSlider speakerVolumeSlider;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JSlider volumeSlider;
+    private javax.swing.JPanel vuMeterPanel;
     // End of variables declaration//GEN-END:variables
 }
