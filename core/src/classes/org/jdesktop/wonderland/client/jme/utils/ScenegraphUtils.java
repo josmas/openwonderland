e88bb96714a240e8b37f2f355a2beafdf6fc680a
bernard_horan <bernard_horan@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2009-12-16 17:25:47 +0000

    Added a couple of helper methods: printChildren() and printParents(). Also added test methods.

--- core/src/classes/org/jdesktop/wonderland/client/jme/utils/ScenegraphUtils.java
+++ core/src/classes/org/jdesktop/wonderland/client/jme/utils/ScenegraphUtils.java
@@ -20,8 +20,11 @@ package org.jdesktop.wonderland.client.jme.utils;
 
 import com.jme.scene.Node;
 import com.jme.scene.Spatial;
-import java.util.ArrayList;
+import com.jme.scene.TriMesh;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 import java.util.logging.Logger;
 import org.jdesktop.wonderland.client.jme.utils.traverser.ProcessNodeInterface;
 import org.jdesktop.wonderland.client.jme.utils.traverser.TreeScan;
@@ -31,8 +34,80 @@ import org.jdesktop.wonderland.common.cell.CellTransform;
  * Various utilties for the JME scene graph
  *
  * @author paulby
+ * @author Bernard Horan
  */
 public class ScenegraphUtils {
+
+    /**
+     * Print the children of the parameter node on the parameter buffer
+     * @param aNode the node whose children are to be printed
+     * @param buffer the buffer that will contain the tree of printed nodes
+     */
+    public static void printChildren(Node aNode, StringBuffer buffer) {
+        printChildren(aNode, buffer, 0);
+    }
+
+    /**
+     * Print the children of the parameter node on the parameter buffer, using
+     * an indent count to produce an indented tree
+     * @param aNode the node whose children are to be printed
+     * @param buffer the buffer that will contain the tree of printed nodes
+     * @param indent a count of how deep this branch is
+     */
+    public static void printChildren(Node aNode, StringBuffer buffer, int indent) {
+        StringBuffer indentBuffer = new StringBuffer();
+        for (int i = 0; i < indent ; i++) {
+            indentBuffer.append('\t');
+        }
+        buffer.append(indentBuffer.toString());
+        buffer.append(aNode.toString());
+        buffer.append('\n');
+        List<Spatial> children = aNode.getChildren();
+        if (children == null) {
+            return;
+        }
+        indentBuffer.append('\t');
+        for (Iterator<Spatial> it = children.iterator(); it.hasNext();) {
+            Spatial spatial = it.next();
+            if (spatial instanceof Node) {
+                printChildren((Node) spatial, buffer, indent +1);
+            } else {
+                buffer.append(indentBuffer.toString());
+                buffer.append(spatial.toString());
+                buffer.append('\n');
+            }
+        }
+    }
+
+    /**
+     * Print the parents of the parameter spatial on the parameter buffer
+     * @param aSpatial the spatial whose parents are to be printed
+     * @param buffer the buffer that will contain the tree of printed spatials
+     */
+    public static void printParents(Spatial aSpatial, StringBuffer buffer) {
+        printParents(aSpatial, buffer, 0);
+    }
+
+    /**
+     * Print the parents of the parameter spatial on the parameter buffer, using
+     * an indent count to indent the printing
+     * @param aSpatial the spatial whose parents are to be printed
+     * @param buffer the buffer that will contain the tree of printed spatials
+     * @param indent a count of how deep the parent is up the tree
+     */
+    public static void printParents(Spatial aSpatial, StringBuffer buffer, int indent) {
+         StringBuffer indentBuffer = new StringBuffer();
+        for (int i = 0; i < indent ; i++) {
+            indentBuffer.append('\t');
+        }
+        buffer.append(indentBuffer.toString());
+        buffer.append(aSpatial.toString());
+        buffer.append('\n');
+        Node parent = aSpatial.getParent();
+        if (parent != null) {
+            printParents(parent, buffer, indent + 1);
+        }
+    }
 
 
     /**
@@ -91,6 +166,62 @@ public class ScenegraphUtils {
         return newChildLocal;
     }
 
+    private static Node createTestNode() {
+        Node rootNode = new Node("Root");
+        for (int i = 100; i <= 500; i +=100) {
+            Node iNode = new Node(String.valueOf(i));
+            rootNode.attachChild(iNode);
+            for (int j = 10; j <= 50; j +=10) {
+                Node jNode = new Node(String.valueOf(i+j));
+                iNode.attachChild(jNode);
+                for (int k = 1; k <= 5; k++) {
+                    TriMesh kMesh = new TriMesh(String.valueOf(i+j+k));
+                    jNode.attachChild(kMesh);
+                }
+            }
+        }
+        return rootNode;
+    }
+
+    private static void testPrintNamedNodes(Node testNode) {
+        System.out.println("TEST: PRINTING NAMED NODES");
+        HashMap<String, Spatial> nodeMap = new HashMap<String, Spatial>();
+        getNamedNodes(testNode, nodeMap);
+        Set<String> mapKeys = nodeMap.keySet();
+        for (String key : mapKeys) {
+            System.out.println("key: " + key + " --> " + nodeMap.get(key));
+        }
+    }
+
+    private static void testNamedNode(Node testNode, String string) {
+        System.out.println("TEST: FINDING NAMED NODE");
+        Spatial found = findNamedNode(testNode, string);
+        if (found != null) {
+            System.out.println("Found spatial for " + string + " --> " + found);
+        } else {
+            System.err.println("Failed to find node for " + string);
+        }
+    }
+
+    private static void testPrintChildren(Node testNode) {
+        System.out.println("TEST: PRINT CHILDREN");
+        StringBuffer buffer = new StringBuffer();
+        printChildren(testNode, buffer);
+        System.out.println(buffer.toString());
+    }
+
+    private static void testPrintParents(Node testNode, String string) {
+        System.out.println("TEST: PRINT PARENTS");
+        Spatial found = findNamedNode(testNode, string);
+        if (found != null) {
+            StringBuffer buffer = new StringBuffer();
+            printParents(found, buffer);
+            System.out.println(buffer.toString());
+        } else {
+            System.err.println("Failed to find node for " + string);
+        }
+    }
+
     /**
      * Given a list of cell transforms, multiply them together and return the
      * 'world' transform of the leaf transform. This mimics the transform calculation in the scene
@@ -127,6 +258,14 @@ public class ScenegraphUtils {
         public Spatial getResult() {
             return result;
         }
+    }
+
+    public static void main(String[] args) {
+        Node testNode = createTestNode();
+        testPrintNamedNodes(testNode);
+        testNamedNode(testNode, "444");
+        testPrintChildren(testNode);
+        testPrintParents(testNode, "444");
     }
 
 }
