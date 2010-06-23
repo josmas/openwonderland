b680bac14ae9a627120e67affe67566be06ede33
jonathankap <jonathankap@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2010-06-23 00:24:12 +0000

    Fixes issue 63  - enable reparenting of X11 apps by preserving server state

--- trunk/modules/foundation/appbase/src/classes/org/jdesktop/wonderland/modules/appbase/server/cell/AppConventionalCellMO.java
+++ trunk/modules/foundation/appbase/src/classes/org/jdesktop/wonderland/modules/appbase/server/cell/AppConventionalCellMO.java
@@ -1,4 +1,22 @@
 /**
+ * Open Wonderland
+ *
+ * Copyright (c) 2010, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
+/**
  * Project Wonderland
  *
  * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
@@ -326,7 +344,11 @@ public abstract class AppConventionalCellMO extends App2DCellMO {
      */
     public void destroy () {
         CellManagerMO.getCellManager().removeCellFromWorld(this);
-        serverState = null;
+
+        // OWL issue #63: preserve the server state so that reparent (which
+        // causes a setLive(false) followed by setLive(true)) works properly
+        //serverState = null;
+
         connectionInfo = null;
     }
 
