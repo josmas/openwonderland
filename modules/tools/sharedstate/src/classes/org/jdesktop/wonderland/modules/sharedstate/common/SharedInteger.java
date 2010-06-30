83ac5c33be5dc95b9e1b2d41362a1977e5b49328
pympnotiq@gmail.com <pympnotiq@gmail.com@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2010-06-30 22:03:03 +0000

    Shared state improvements:  - allow generic types to be stored in shared maps

--- modules/tools/sharedstate/src/classes/org/jdesktop/wonderland/modules/sharedstate/common/SharedInteger.java
+++ modules/tools/sharedstate/src/classes/org/jdesktop/wonderland/modules/sharedstate/common/SharedInteger.java
@@ -1,4 +1,22 @@
 /**
+ * Open Wonderland
+ *
+ * Copyright (c) 2010, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
+/**
  * Project Wonderland
  *
  * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
@@ -17,11 +35,17 @@
  */
 package org.jdesktop.wonderland.modules.sharedstate.common;
 
+import javax.xml.bind.annotation.XmlRootElement;
+import org.jdesktop.wonderland.common.cell.state.annotation.ServerState;
+
 /**
  * Shared data representation of an integer
  * @author jkaplan
+ * @author jagwire
  */
-public class SharedInteger implements SharedData {
+@ServerState
+@XmlRootElement(name="shared-integer")
+public class SharedInteger extends SharedData {
     private static final long serialVersionUID = 1L;
     private int value;
 
