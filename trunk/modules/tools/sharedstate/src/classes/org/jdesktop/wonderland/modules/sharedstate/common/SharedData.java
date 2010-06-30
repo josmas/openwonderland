0755c1b5c16c74ac79004db86e18d9f35c3c842b
pympnotiq@gmail.com <pympnotiq@gmail.com@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2010-06-30 22:03:03 +0000

    Shared state improvements:  - allow generic types to be stored in shared maps

--- trunk/modules/tools/sharedstate/src/classes/org/jdesktop/wonderland/modules/sharedstate/common/SharedData.java
+++ trunk/modules/tools/sharedstate/src/classes/org/jdesktop/wonderland/modules/sharedstate/common/SharedData.java
@@ -1,4 +1,22 @@
 /**
+ * Open Wonderland
+ *
+ * Copyright (c) 2010, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
+/**
  * Project Wonderland
  *
  * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
@@ -16,15 +34,17 @@
  * this code.
  */
 package org.jdesktop.wonderland.modules.sharedstate.common;
-
+import org.jdesktop.wonderland.common.cell.state.CellState;
 import java.io.Serializable;
 
 /**
- * A tagging interface for components of a shared map. Note that arbitrary
- * classes cannot implement this interface -- there are assumptions in
- * the persistence of SharedData objects that only allow certain classes
- * to be persisted.
- * @author jkaplan
+ * The base class for data shared via the shared state component.  In order
+ * to be used in a shared map, data types must extend SharedData, be properly
+ * serializable via JAXB (ie annotated with @XmlRootElement) and be
+ * annotated with @ServerState.
+ * 
+ * @author Jonathan Kaplan <jonathankap@gmail.com>
+ * @author jagwire
  */
-public interface SharedData extends Serializable {
+public abstract class SharedData implements Serializable, CellState {
 }
