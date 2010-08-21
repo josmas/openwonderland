a511dfc95d1a2aa320bd61de4d9cfcd27a7b14a9
jonathankap <jonathankap@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2010-08-21 18:29:05 +0000

    Fix for disappearing skybox after warm start

--- trunk/core/src/classes/org/jdesktop/wonderland/server/cell/CellPersistence.java
+++ trunk/core/src/classes/org/jdesktop/wonderland/server/cell/CellPersistence.java
@@ -1,4 +1,22 @@
 /**
+ * Open Wonderland
+ *
+ * Copyright (c) 2010, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
+/**
  * Project Wonderland
  *
  * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
@@ -82,7 +100,9 @@ public class CellPersistence {
         doInsert(cell, universe);
 
         // record a new root cell with the universe
-        universe.addRootToUniverse(cell);
+        if (!cellID.equals(CellID.getEnvironmentCellID())) {
+            universe.addRootToUniverse(cell);
+        }
 
         // all set
         return true;
