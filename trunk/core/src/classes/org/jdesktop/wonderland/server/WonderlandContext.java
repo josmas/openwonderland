9dc84fffd3814911ff9d56587af2eccfde474152
jonathankap <jonathankap@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2010-11-18 20:40:45 +0000

    Rearrange startup order so that server plugins can depend on the UserManager

--- trunk/core/src/classes/org/jdesktop/wonderland/server/WonderlandContext.java
+++ trunk/core/src/classes/org/jdesktop/wonderland/server/WonderlandContext.java
@@ -1,4 +1,22 @@
 /**
+ * Open Wonderland
+ *
+ * Copyright (c) 2010, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
+/**
  * Project Wonderland
  *
  * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
@@ -37,10 +55,12 @@ public class WonderlandContext {
      * Initialize the WonderlandContext, create all singletons
      */
     static void intialize() {
+        // UserManager has no dependencies, so initialize it first
+        UserManager.initialize();
+
         // initialize the comms manager
         CommsManagerFactory.initialize();
         CellManagerMO.initialize();
-        UserManager.initialize();
     }
     
     /**
