337f13b7c872ffa94140b9a0b4b09cf0ef11b09b
jonathankap <jonathankap@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2010-12-23 19:31:54 +0000

    First pass at tooltips and other popups in the HUD  - added an interface in appbase to let windows handle their own popups  - added new HUDWindow type that handles popups  - add popups as normal HUD windows
    Update issue 147
     - partial fix allows tooltips, combo boxes partially working

--- trunk/modules/foundation/hud/src/classes/org/jdesktop/wonderland/modules/hud/client/HUDView2DDisplayer.java
+++ trunk/modules/foundation/hud/src/classes/org/jdesktop/wonderland/modules/hud/client/HUDView2DDisplayer.java
@@ -1,3 +1,21 @@
+/**
+ * Open Wonderland
+ *
+ * Copyright (c) 2010, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
 /*
  * Project Wonderland
  * 
@@ -19,6 +37,7 @@ package org.jdesktop.wonderland.modules.hud.client;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import org.jdesktop.wonderland.client.hud.HUD;
 import org.jdesktop.wonderland.modules.appbase.client.Window2D;
 import org.jdesktop.wonderland.modules.appbase.client.view.View2D;
 import org.jdesktop.wonderland.modules.appbase.client.view.View2DDisplayer;
@@ -31,17 +50,20 @@ import org.jdesktop.wonderland.modules.appbase.client.view.View2DDisplayer;
 public class HUDView2DDisplayer implements View2DDisplayer {
 
     // HUD views displayed by this displayer
-    private LinkedList<HUDView2D> views = new LinkedList<HUDView2D>();
+    private final LinkedList<HUDView2D> views = new LinkedList<HUDView2D>();
+    private final HUD hud;
 
-    public HUDView2DDisplayer() {
+    public HUDView2DDisplayer(HUD hud) {
         super();
+
+        this.hud = hud;
     }
 
     /**
      * {@inheritDoc}
      */
     public HUDView2D createView(Window2D window) {
-        HUDView2D view = new HUDView2D(this, window);
+        HUDView2D view = new HUDView2D(hud, this, window);
 
         // invisible by default
         view.setVisibleUser(false);
