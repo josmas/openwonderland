01b25b0e3b9dcdd52ad436e5941ad585f948339f
jonathankap <jonathankap@users.noreply.github.com>
2011-01-04 20:32:40 +0000

    Add missing copyright headers

--- web/server/src/classes/org/jdesktop/wonderland/webserver/WebLibDeployer.java
+++ web/server/src/classes/org/jdesktop/wonderland/webserver/WebLibDeployer.java
@@ -1,4 +1,22 @@
 /**
+ * Open Wonderland
+ *
+ * Copyright (c) 2010 - 2011, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
+/**
  * Project Wonderland
  *
  * Copyright (c) 2004-2010, Sun Microsystems, Inc., All Rights Reserved
