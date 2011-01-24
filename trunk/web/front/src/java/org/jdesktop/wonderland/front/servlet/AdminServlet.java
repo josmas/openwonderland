652b7e664627ae9efbf5adaa60681bb579ffc630
jonathankap <jonathankap@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2011-01-24 18:05:58 +0000

    New client properties editor in web UI  - set screen size, audio mode and initial location  - properties editor for advanced editing

--- trunk/web/front/src/java/org/jdesktop/wonderland/front/servlet/AdminServlet.java
+++ trunk/web/front/src/java/org/jdesktop/wonderland/front/servlet/AdminServlet.java
@@ -1,4 +1,22 @@
 /**
+ * Open Wonderland
+ *
+ * Copyright (c) 2010 - 2011, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
+/**
  * Project Wonderland
  *
  * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
@@ -163,7 +181,13 @@ public class AdminServlet extends HttpServlet implements ServletContextListener 
                 "Manage Modules", "/wonderland-web-modules/editor");
         modules.setFilter(AdminRegistration.ADMIN_FILTER);
         registry.add(modules);
-       
+
+        // Register the client properties management page
+        AdminRegistration client = new AdminRegistration("client",
+                "Client Settings", "/wonderland-web-front/config/settings");
+        client.setFilter(AdminRegistration.ADMIN_FILTER);
+        registry.add(client);
+
         // add the registry to the context
         ServletContext sc = sce.getServletContext();
         sc.setAttribute(AdminRegistration.ADMIN_REGISTRY_PROP, registry);
