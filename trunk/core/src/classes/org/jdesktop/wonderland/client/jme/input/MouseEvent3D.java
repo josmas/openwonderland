abed1ace7bad9d5549236afda9fe231b1345add1
jonathankap <jonathankap@d1a7cd20-233f-11df-bff0-d338b04bc40e>
2012-02-28 01:38:53 +0000

    Fix NPE getting mouse intersection point  - check for null before getting position  - fix developed at Wonderland Wednesday
    Fixes issue 246

--- trunk/core/src/classes/org/jdesktop/wonderland/client/jme/input/MouseEvent3D.java
+++ trunk/core/src/classes/org/jdesktop/wonderland/client/jme/input/MouseEvent3D.java
@@ -1,4 +1,22 @@
 /**
+ * Open Wonderland
+ *
+ * Copyright (c) 2012, Open Wonderland Foundation, All Rights Reserved
+ *
+ * Redistributions in source code form must reproduce the above
+ * copyright and this condition.
+ *
+ * The contents of this file are subject to the GNU General Public
+ * License, Version 2 (the "License"); you may not use this file
+ * except in compliance with the License. A copy of the License is
+ * available at http://www.opensource.org/licenses/gpl-license.php.
+ *
+ * The Open Wonderland Foundation designates this particular file as
+ * subject to the "Classpath" exception as provided by the Open Wonderland
+ * Foundation in the License file that accompanied this code.
+ */
+
+/**
  * Project Wonderland
  *
  * Copyright (c) 2004-2009, Sun Microsystems, Inc., All Rights Reserved
@@ -144,7 +162,7 @@ public abstract class MouseEvent3D extends InputEvent3D {
      * which were calculated by the input system.
      */
     public Vector3f getIntersectionPointWorld() {
-        if (pickDetails == null) {
+        if (pickDetails == null || pickDetails.getPosition() == null) {
             return null;
         } else {
 	    return new Vector3f(pickDetails.getPosition());
@@ -156,7 +174,7 @@ public abstract class MouseEvent3D extends InputEvent3D {
      * pick details which were calculated by the input system.
      */
     public Vector3f getIntersectionPointLocal() {
-        if (pickDetails == null) {
+        if (pickDetails == null || pickDetails.getPosition() == null) {
             return null;
         } else {
             Vector3f posWorld = pickDetails.getPosition();
